43ed6a272765ec00caf6b78ffe5fe080b80be08c
sfarrens <samuel.farrens@gmail.com>
2017-03-28 16:24:37 +0200

    test

--- modules/ppe_package/config/launch_ppe_smp.cmd
+++ modules/ppe_package/config/launch_ppe_smp.cmd
@@ -2,6 +2,7 @@
 
 # Set path to PPE modules
 setenv PPE_DIR $HOME/pipeline/ShapePipe/modules/ppe_package
+setenv PPE_CONFIG_DIR ${PPE_DIR}/config/
 
 # Run PPE
-python ${PPE_DIR}/ppe/ppe_SMP.py -d ${PPE_DIR}/config/ -c ppe_config_smp.cfg
+python ${PPE_DIR}/ppe/ppe_SMP.py -d ${PPE_CONFIG_DIR} -c ppe_config_smp.cfg
