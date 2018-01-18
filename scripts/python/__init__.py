ddc72da2cc38277faeec5facfed30b1a94ea1ae8
Samuel Farrens <samuel.farrens@gmail.com>
2018-01-18 17:43:16 +0100

    Added scripts directory

---
+++ scripts/python/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""ShapePipe Python Modules
+
+These modules provide functionality to the executable Python scripts in the
+`bin` directory.
+
+:Author: Samuel Farrens <samuel.farrens@cea.fr>
+
+"""
---
+++ scripts/python/lib/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""ShapePipe Python Library
+
+This module contains generic python modules that can be used for various
+projects.
+
+:Author: Samuel Farrens <samuel.farrens@cea.fr>
+
+"""
