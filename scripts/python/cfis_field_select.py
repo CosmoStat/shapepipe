aa426a6dca4942e2336829c6af277adf03a39dcc
Martin Kilbinger <martin.kilbinger@cea.fr>
2018-01-19 11:27:27 +0100

    Added CFIS scripts.

---
+++ scripts/python/cfis_field_select.py
@@ -0,0 +1,540 @@
+#!/usr/bin/env python
+
+
+"""Script cfis_field_select.py
+
+Handling and selecting CFIS fields and pointings.
+
+:Authors: Martin Kilbinger
+
+:Date: 19/01/2018
+"""
+
+
+# Compability with python2.x for x>6
+from __future__ import print_function
+
+
+import sys
+import os
+import re
+import copy
+import glob
+
+import numpy as np
+import pylab as plt
+
+from astropy.io import ascii
+from astropy.table import Table, Column
+from astropy import units
+from astropy.coordinates import Angle
+
+from optparse import OptionParser, IndentedHelpFormatter
+from optparse import OptionGroup
+
+import cfis
+from cfis import unitdef
+import stuff
+
+
+#unitdef = 'degree'
+
+
+
+def get_image_list(inp, band, image_type, verbose=False):
+    """Return list of images.
+
+    Parameters
+    ----------
+    input: string
+        file name or direcory path
+    verbose: bool, optional
+        verbose output if True, default=False
+
+    Return
+    ------
+    img_list: list of strings
+        image list
+    """
+
+    # Get file list
+    if os.path.isfile(inp):
+        inp_type  = 'file'
+        file_list = cfis.read_list(inp)
+
+    elif os.path.isdir(inp):
+        inp_type  = 'dir'
+        file_list = glob.glob(inp)
+
+    else:
+        file_list = None
+
+
+    # Filter file list to match CFIS image pattern
+    img_list = []
+    pattern = cfis.get_file_pattern('', band, image_type)
+
+    for f in file_list:
+
+        m = re.findall(pattern, f)
+        if len(m) != 0:
+            img_list.append(m[0])
+
+    if verbose == True and len(img_list) > 0:
+        print('{} image files found in input {} \'{}\''.format(len(img_list), inp_type, inp))
+
+    return img_list
+
+
+
+def find_image_at_coord(images, coord, band, type, verbose=False):
+
+    ra, dec   = cfis.get_Angle(coord)
+
+    if verbose == True:
+        print('Looking for image at coordinates {}, {}'.format(ra, dec))
+
+    if type == 'tiles':
+        nix, niy  = cfis.get_tile_number_from_coord(ra, dec, return_type=int)
+        tile_name = cfis.get_tile_name(nix, niy, band)
+        im_found = None
+
+        for im in images:
+            if im == tile_name:
+                im_found = im
+                break
+
+        if im_found != None:
+                pass
+        else:
+            if verbose == True:
+                print('Image with numbers ({}, {}) not found'.format(nix, niy))
+
+    else:
+        stuff.error('Only implemented for type=tiles')
+
+    return im_found
+
+
+
+def find_images_in_area(images, angles, band, type, verbose=False):
+
+    if verbose == True:
+        print('Looking for all images within coordinates ', angles)
+
+    found = []
+    for img in images:
+        nix, niy = cfis.get_tile_number(img)
+        ra, dec  = cfis.get_tile_coord_from_nixy(nix, niy)
+        if ra.is_within_bounds(angles[0].ra, angles[1].ra) \
+            and dec.is_within_bounds(angles[0].dec, angles[1].dec):
+            img = cfis.image(img, ra, dec)
+            found.append(img)
+
+    if verbose == True:
+        print('{} images found in area'.format(len(found)))
+
+    return found
+
+
+def get_coord_at_image(number, type, verbose=False):
+
+
+    if type == 'tiles':
+	# TODO: Read entire image name, not just the two tile numbers
+        nix, niy = stuff.my_string_split(number, num=2, stop=True)
+
+        if verbose == True:
+            print('Looking for coordinates for tile with numbers ({},{})'.format(nix, niy))
+
+        ra, dec  = cfis.get_tile_coord_from_nixy(nix, niy)
+    else:
+        tuff.error('Image type {} not implemented yet'.format(type))
+
+    return ra, dec
+
+    
+
+def test_tile_number():
+
+    #ra  = Angle(180, unit=units.deg)
+    #dec = Angle(35, unit=units.deg)
+    ra  = Angle('10:44:00.0 hours')
+    dec = Angle('30:00:00 degrees')
+    print(ra, dec)
+    print(ra.deg, dec.deg)
+
+    nix, niy = cfis.get_tile_number_from_coord(ra, dec)
+    print(nix, niy)
+
+    ra, dec = cfis.get_tile_coord_from_nixy(nix, niy)
+    print(ra, dec)
+
+
+def square_from_centre(x, y, dx, dy):
+    """Return coordinate vectors of corners cx, cy that define a closed square for plotting.
+    """
+
+    cx = [x-dx, x+dx, x+dx, x-dx, x-dx]
+    cy = [y-dy, y-dy, y+dy, y+dy, y-dy]
+
+    return cx, cy
+
+
+
+def square_from_corners(ang0, ang1):
+    """Return coordinate vectors of corners cx, cy that define a closed square for plotting.
+    """
+
+    cx = [ang0.ra, ang1.ra, ang1.ra, ang0.ra, ang0.ra]
+    cy = [ang0.dec, ang0.dec, ang1.dec, ang1.dec, ang0.dec]
+
+    cxd = [getattr(i, unitdef) for i in cx]
+    cyd = [getattr(i, unitdef) for i in cy]
+
+    return cxd, cyd
+
+
+
+def plot_area(images, angles, outbase):
+    """Plot images within area.
+
+    Parameters
+    ----------
+    images: array of cfis.image
+        images
+    angles: array(SkyCoord, 2)
+        Corner coordinates of area rectangle
+    outbase: string
+        output file name base
+    """
+
+    if outbase is None:
+        outname = 'plot.pdf'
+    else:
+        outname = '{}.pdf'.format(outbase)
+
+    fig, ax = plot_init()
+
+    # Field center
+    ra_c  = sum([img.ra for img in images])/len(images)
+    dec_c = sum([img.dec for img in images])/len(images)
+    plt.plot(ra_c, dec_c, 'or', mfc='none', ms=3)
+
+    for img in images:
+        # Image center
+        x  = img.ra.degree
+        y  = img.dec.degree
+        plt.plot(x, y, 'b.', markersize=1)
+
+        # Image boundary
+        dx = 0.25
+        dy = 0.25
+        cx, cy = square_from_centre(x, y, dx, dy)
+        plt.plot(cx, cy, 'g-', linewidth=0.5) 
+
+    # Area border
+    cx, cy = square_from_corners(angles[0], angles[1])
+    plt.plot(cx, cy, 'r-.', linewidth=0.5)
+
+    plt.xlabel('R.A. [degree]')
+    plt.ylabel('Declination [degree]')
+    if outbase is not None:
+        plt.title(outbase)
+
+    border = 5
+    plt.xlim(angles[0].ra.degree - border, angles[1].ra.degree + border)
+    plt.ylim(angles[0].dec.degree - border, angles[1].dec.degree + border)
+    plt.axis('equal')
+
+    print('Saving plot to {}'.format(outname))
+    plt.savefig(outname)
+
+
+
+def plot_init():
+
+    fs = 12
+    fig = plt.figure()
+
+    ax = plt.gca()
+    ax.yaxis.label.set_size(fs)
+    ax.xaxis.label.set_size(fs)
+
+    plt.tick_params(axis='both', which='major', labelsize=fs)
+
+    plt.rcParams.update({'figure.autolayout': True})
+
+    return fig, ax
+
+
+
+def params_default():
+    """Set default parameter values.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    p_def: class tuff.param
+        parameter values
+    """
+
+    p_def = stuff.param(
+        input  = '.',
+        mode  = 'c',
+        band  = 'r',
+        type  = 'tiles',
+    )
+
+    return p_def
+
+
+
+def parse_options(p_def):
+    """Parse command line options.
+
+    Parameters
+    ----------
+    p_def: class tuff.param
+        parameter values
+
+    Returns
+    -------
+    options: tuple
+        Command line options
+    args: string
+        Command line string
+    """
+
+    usage  = "%prog [OPTIONS]"
+    parser = OptionParser(usage=usage, formatter=stuff.IndentedHelpFormatterWithNL())
+
+    # I/O
+    parser.add_option('-i', '--input', dest='input', type='string', default=p_def.input,
+         help='input image list, can be ascii file or directory path')
+    parser.add_option('-o', '--outbase', dest='outbase', type='string', default=None,
+         help='output file name base (\'.txt\' is added), default=stdout')
+    parser.add_option('', '--plot', dest='plot', action='store_true',
+         help='create plots')
+
+    # Job control
+    parser.add_option('-m', '--mode', dest='mode', type='string', default=None,
+         help='run mode, one of [n|c|a]:\n'
+	      ' n: search image number given a coordinate (--coord)\n'
+	      ' c: search for image coord. given a number (--number)\n'
+	      ' a: area search, search images within area (--area)\n')
+
+    # Field and image options
+    parser.add_option('-b', '--band', dest='band', type='string', default=p_def.band,
+        help='band, one of \'r\' (default)|\'u\'')
+    parser.add_option('-t', '--type', dest='type', type='string', default=p_def.type,
+        help='data type, one of \'tiles\' (default)| \'cat\'|\'weight\'|\'raw\'')
+
+    parser.add_option('', '--coord', dest='coord', type='string', default=None,
+        help='(white-space or \'_\' separated) string of input coordinates, as astropy.coordinates.Angle')
+    parser.add_option('', '--number', dest='number', type='string', default=None,
+        help='input image number')
+    parser.add_option('', '--area', dest='area', type='string', default=None,
+        help='area corner coordinates ra0_dec0_ra1_dec1')
+
+    # Monitoring
+    parser.add_option('-v', '--verbose', dest='verbose', action='store_true', help='verbose output')
+
+    options, args = parser.parse_args()
+
+    return options, args
+
+
+
+def check_options(options):
+    """Check command line options.
+
+    Parameters
+    ----------
+    options: tuple
+        Command line options
+
+    Returns
+    -------
+    erg: bool
+        Result of option check. False if invalid option value.
+    """
+
+    if options.mode is None:
+        stuff.error('Run mode not given (option \'-m\')')
+
+    if options.mode == 'n' and options.coord == None:
+        stuff.error('No input coordinates given (option \'--coord\')')
+    if options.mode == 'c' and options.number == None:
+        stuff.error('No input image number given (option \'--number\')')
+    if options.mode == 'a' and options.area == None:
+        stuff.error('No input area given (option \'--area\')')
+
+    see_help = 'See option \'-h\' for help.'
+
+    return True
+
+
+
+def update_param(p_def, options):
+    """Return default parameter, updated and complemented according to options.
+    
+    Parameters
+    ----------
+    p_def:  class stuff.param
+        parameter values
+    optiosn: tuple
+        command line options
+    
+    Returns
+    -------
+    param: class stuff.param
+        updated paramter values
+    """
+
+    param = copy.copy(p_def)
+
+    # Update keys in param according to options values
+    for key in vars(param):
+        if key in vars(options):
+            setattr(param, key, getattr(options, key))
+
+    # Add remaining keys from options to param
+    for key in vars(options):
+        if not key in vars(param):
+            setattr(param, key, getattr(options, key))
+
+    # Do extra stuff if necessary
+    if param.outbase is not None:
+        param.fout = open('{}.txt'.format(param.outbase), 'w')
+    else:
+        param.fout = sys.stdout
+
+    return param
+
+
+
+def run_mode(images, param):
+    """Performs action according to run mode.
+
+    Parameters
+    ----------
+    images: list of strings
+        list of images
+    param: class stuff.param
+        parameter values
+
+    Returns
+    -------
+    ex: int
+        exit code, 0 if successful
+    """
+
+    # Default value
+    ex = 1
+
+    if param.mode == 'n':
+
+        # Image number search: Return number of tile that covers input coordinate
+        image_name = find_image_at_coord(images, param.coord, param.band, param.type, verbose=param.verbose)
+        if image_name != None:
+            print('# name')
+            print(image_name, file=param.fout) 
+            ex =  0
+
+    elif param.mode == 'c':
+
+        # Coordinate search: Return coordinate covered by tile with input number
+        ra, dec = get_coord_at_image(param.number, param.type, verbose=param.verbose)
+        print('# ra[{0}] dec[{0}]'.format(unitdef), file=param.fout)
+        print(getattr(ra, unitdef), getattr(dec, unitdef), file=param.fout)
+        ex = 0
+
+    elif param.mode == 'a':
+
+        # Area search: Return tiles within input area
+        ex = 0
+        angles = cfis.get_Angle_arr(param.area, num=4, verbose=param.verbose)
+        images = find_images_in_area(images, angles, param.band, param.type, verbose=param.verbose)
+        print('# Name ra[{0}] dec[{0}]'.format(unitdef), file=param.fout)
+        for img in images:
+            print('{} {} {}'.format(img.name, getattr(img.ra, unitdef), getattr(img.dec, unitdef)), file=param.fout)
+        if param.plot == True:
+            if param.verbose == True:
+                print('Creating plots')
+            plot_area(images, angles, param.outbase)
+        ex = 0
+
+    else:
+
+        stuff.error('Unknown run mode \'{}\''.format(param.mode))
+
+
+    return ex
+
+
+
+def main(argv=None):
+    """Main program.
+    """
+
+    
+    # Set default parameters
+    p_def = params_default()
+
+    # Command line options
+    options, args = parse_options(p_def)
+    # Without option parsing, this would be: args = argv[1:]
+
+    if check_options(options) is False:
+        return 1
+
+    param = update_param(p_def, options)
+
+
+    # Save calling command
+    stuff.log_command(argv)
+    if param.verbose:
+        stuff.log_command(argv, name='sys.stderr')
+
+
+    if param.verbose is True:
+        print('Start program {}'.format(os.path.basename(argv[0])))
+
+
+    ### Start main program ###
+
+    images = get_image_list(param.input, param.band, param.type, verbose=param.verbose)
+
+
+    # Check wether images have been found, if necessary for run mode
+    if param.mode != 'c':
+        if images is None:
+            stuff.error('Input {} not found, neither existing file nor directory'.format(param.input))
+        if len(images) == 0:
+            stuff.error('No image files found in input \'{}\''.format(param.input))
+
+
+    # Run
+    ex = run_mode(images, param)
+
+
+    if param.outbase is not None:
+        param.fout.close()
+    
+
+    ### End main program
+
+    if param.verbose is True:
+        print('Finish program {}'.format(os.path.basename(argv[0])))
+
+    return ex
+
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
+
---
+++ scripts/python/lib/cfis/cfis.py
@@ -0,0 +1,268 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Module cfis.py
+
+CFIS module
+
+:Authors: Martin Kilbinger
+
+:Date: 19/01/2018
+"""
+
+import re
+import sys
+
+import numpy as np
+
+from astropy import units
+from astropy.coordinates import Angle
+from astropy.coordinates import SkyCoord
+
+import mkstuff
+
+
+unitdef = 'degree'
+
+
+class image():
+
+    def __init__(self, name, ra, dec):
+        self.name = name
+        self.ra   = ra
+        self.dec  = dec
+
+
+
+def get_file_pattern(pattern, band, type):
+    """Return file pattern of CFIS image file.
+    """
+
+    if pattern == '':
+        if type == 'raw':
+            pattern_base = '\d{7}p'
+        else:
+            pattern_base  = 'CFIS.*\.{}'.format(band)
+    else:
+        pattern_base = pattern
+
+    if type == 'raw':
+        pattern  = '{}\.fits.fz'.format(pattern_base)
+    elif type == 'tiles':
+        pattern = '{}\.fits'.format(pattern_base)
+    elif type == 'cat':
+        pattern = '{}\.cat'.format(pattern_base)
+    elif type == 'weight':
+        pattern = '{}\.weight\.fits\.fz'.format(pattern_base)
+    else:
+        mkstuff.error('Invalid type \'{}\''.format(type))
+
+    return pattern
+
+
+def get_tile_number_from_coord(ra, dec, return_type=str):
+    """Return CFIS stacked image tile number covering input coordinates.
+        This is the inverse to get_tile_coord_from_nixy.
+
+    Parameters
+    ----------
+    ra: Angle
+        right ascension
+    dec: Angle
+        declination
+    return type: <type 'type'>
+	return type, int or str
+
+    Returns
+    -------
+    nix: string
+        tile number for x
+    niy: string
+        tile number for y
+    """
+
+    y = (dec.degree + 90) * 2.0
+    yi = int(np.rint(y))
+
+    x = ra.degree * np.cos(dec.radian) * 2.0
+    #x = ra.degree * 2 * np.cos(y/2 / 180 * np.pi - np.pi/2)
+    xi = int(np.rint(x))
+    if xi == 720:
+        xi = 0
+
+    if return_type == str:
+        nix = '{:03d}'.format(xi)
+        niy = '{:03d}'.format(yi)
+    elif return_type == int:
+        nix = xi
+        niy = yi
+    else:
+        mkstuff.error('Invalid return type {}'.format(return_type))
+
+    return nix, niy
+
+
+def get_tile_coord_from_nixy(nix, niy):
+    """ Return coordinates corresponding to tile with number (nix,niy).
+        This is the inverse to get_tile_number_from_coord.
+    Parameters
+    ----------
+    nix: string
+        tile number for x
+    niy: string
+        tile number for y
+
+    Returns
+    -------
+    ra: Angle
+        right ascension
+    dec: Angle
+        declination
+    """
+
+    xi = int(nix)
+    yi = int(niy)
+
+    d = (yi/2.0 - 90)
+    dec = Angle('{} degrees'.format(d))
+    r = xi / 2.0 / np.cos(dec.radian)
+    ra = Angle('{} degrees'.format(r))
+
+    return ra, dec
+
+
+
+def get_tile_name(nix, niy, band):
+    """Return tile name for given tile numbers.
+
+   Parameters
+   ----------
+    nix: string
+        tile number for x
+    niy: string
+        tile number for y
+    band: string
+        band, one in 'r' or 'u'
+
+    Returns
+    -------
+    tile_name: string
+        tile name
+    """
+
+    if type(nix) is int and type(niy) is int:
+    	tile_name = 'CFIS.{:03d}.{:03d}.{}.fits'.format(nix, niy, band)
+
+    elif type(nix) is str and type(niy) is str:
+    	tile_name = 'CFIS.{}.{}.{}.fits'.format(nix, niy, band)
+
+    else:
+        mkstuff.error('Invalid type for input tile numbers {}, {}'.format(nix, niy))
+
+
+    return tile_name
+
+
+
+def get_tile_number(tile_name):
+    """Return tile number of given image tile name
+
+    Parameters
+    ----------
+    tile_name: string
+        tile name
+
+    Returns
+    -------
+    nix: string
+        tile number for x
+    niy: string
+        tile number for y
+    """
+
+    m = re.search('CFIS\.(\d{3})\.(\d{3})', tile_name)
+    if m == None or len(m.groups()) != 2:
+        mkstuff.error('Image name \'{}\' does not match tile name syntax'.format(tile_name))
+
+    nix = m.groups()[0]
+    niy = m.groups()[1]
+
+    return nix, niy
+    
+
+
+def get_Angle(str_coord):
+    """Return Angles ra, dec from coordinate string
+
+    Parameters
+    ----------
+    str_coord: string
+        string of input coordinates
+
+    Returns
+    -------
+    ra: Angle
+        right ascension
+    dec: Angle
+        declination
+    """
+
+    ra, dec = mkstuff.my_string_split(str_coord, num=2, stop=True)
+
+    return Angle(ra), Angle(dec)
+
+
+
+def get_Angle_arr(str_coord, num=-1, verbose=False):
+    """Return array of Angles from coordinate string
+
+    Parameters
+    ----------
+    str_coord: string
+        string of input coordinates
+    num: int, optional, default=-1
+        expected number of coordinates (even number)
+    verbose: bool, optional, default=False
+        verbose output
+
+    Returns
+    -------
+    angles: array of SkyCoord
+        array of sky coordinates (pairs ra, dec)
+    """
+
+    angles_mixed = mkstuff.my_string_split(str_coord, num=num, verbose=verbose, stop=True)
+    n = len(angles_mixed)
+    n = int(n / 2)
+
+    angles = []
+    for i in range(n):
+        c = SkyCoord(angles_mixed[2*i], angles_mixed[2*i+1])
+        angles.append(c)
+
+    return angles
+
+
+
+def read_list(fname):
+    """Read list of from ascii file.
+
+    Parameters
+    ----------
+    fname: string
+        ascii file name
+
+    Returns
+    -------
+    file_list: list of strings
+        list of file name
+    """
+
+    f = open(fname, 'rU')
+    file_list = [x.strip() for x in f.readlines()]
+    f.close()
+
+    file_list.sort()
+    return file_list
+
+
