ecb3175e4dff26c073061ca546ae4c47aea65240
Axel <axel.guinot.09@orange.fr>
2018-02-13 14:47:35 +0100

    New module shapelens

---
+++ modules/shapelens_package/shapelens/info.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""INFO
+
+Set the package information
+
+:Authors: Samuel Farrens
+
+:Date: 31/10/2017 (Happy Halloween!)
+
+"""
+
+__version__ = "1.0.0"
+__whoami__ = "shapelens"
