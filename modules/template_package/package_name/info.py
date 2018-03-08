c4d1895b721d06e03f45d0ffab3e93e820c605d1
Samuel Farrens <samuel.farrens@gmail.com>
2018-03-08 10:33:26 +0100

    updated template help with package dependencies

--- modules/template_package/package_name/info.py
+++ modules/template_package/package_name/info.py
@@ -12,3 +12,13 @@ Set the package information
 
 __version__ = "1.0.0"
 __whoami__ = "package_name"
+
+# Python dependencies
+pipe_depend = ['mpfg', 'mpfx', 'scatalog', 'sconfig', 'slogger']
+external_depend = ['numpy']
+
+__python_depend__ = pipe_depend + external_depend
+
+# System dependencies
+
+__system_depend__ = ['diff']
