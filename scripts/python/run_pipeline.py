458ecec298b8b17b0e144dbaeb1980665921ef11
Martin Kilbinger <martin.kilbinger@cea.fr>
2018-04-16 14:02:38 +0200

    Added _bright_objects to SETools selection

---
+++ scripts/python/run_pipeline.py
@@ -0,0 +1,20 @@
+
+"""
+Call each module
+	- check whether required executables are present
+	- check whether input files are present
+	  if not prepare module
+	- check config files, pointing to right dirs? 
+	- run
+	- ask to adopt/not adopt/postpone run
+
+Adpot last run
+	- create link output/<module>/adopt
+
+Prepare module
+	- in data create links to previous module's adopted run results
+
+Requires on input (config)
+	- List of modules (for each pipeline sequence mode)
+
+"""
