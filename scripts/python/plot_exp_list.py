c31e0803afdd34128f3d32703cba1320cf96e3ed
Martin Kilbinger <martin.kilbinger@cea.fr>
2018-12-13 21:20:43 +0100

    script to plot exposure geometry

--- scripts/python/plot_exp_list.py
+++ scripts/python/plot_exp_list.py
@@ -1,16 +1,23 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
 from astropy.io import ascii
+
 from generic import plot
-import pylab as plt
 
 dat = ascii.read('exp_footprints.txt')
 ax = plot.plot_init()
 lw = 0.25
 
 
+col = 'g'
 for d in dat:
-    cx = [d['col2'], d['col4'], d['col6'], d['col8']]
-    cy = [d['col3'], d['col5'], d['col7'], d['col9']]
-    plt.plot(cx, cy, '-', linewidth=lw)
+    cx = [d['col2'], d['col4'], d['col6'], d['col8'], d['col2']]
+    cy = [d['col3'], d['col5'], d['col7'], d['col9'], d['col3']]
+    plt.plot(cx, cy, '{}-'.format(col), linewidth=lw)
+    plt.text(np.mean(cx), np.mean(cy), str(d['col1']), color=col, fontsize=3,
+             horizontalalignment='center', verticalalignment='center')
  
 
 plt.savefig('exp_footprints.pdf')
