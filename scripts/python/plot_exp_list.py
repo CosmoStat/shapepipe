2f43237ca2e800bbcfad05a31692b58898fe941c
Martin Kilbinger <martin.kilbinger@cea.fr>
2018-12-14 16:44:17 +0100

    plotting colors

--- scripts/python/plot_exp_list.py
+++ scripts/python/plot_exp_list.py
@@ -10,13 +10,18 @@ dat = ascii.read('exp_footprints.txt')
 ax = plot.plot_init()
 lw = 0.25
 
+exp_names = np.unique(dat['name'])
+colors = plot.color('tableau20')
+col    = {}
+for e in exp_names:
+    col[e] = colors.next()
 
-col = 'g'
 for d in dat:
-    cx = [d['col2'], d['col4'], d['col6'], d['col8'], d['col2']]
-    cy = [d['col3'], d['col5'], d['col7'], d['col9'], d['col3']]
-    plt.plot(cx, cy, '{}-'.format(col), linewidth=lw)
-    plt.text(np.mean(cx), np.mean(cy), str(d['col1']), color=col, fontsize=3,
+    cx = [d['cx1'], d['cx2'], d['cx3'], d['cx4'], d['cx1']]
+    cy = [d['cy1'], d['cy2'], d['cy3'], d['cy4'], d['cy1']]
+    c  = col[d['name']]
+    plt.plot(cx, cy, '-', color=c, linewidth=lw)
+    plt.text(np.mean(cx), np.mean(cy), str(d['num']), color=c, fontsize=3,
              horizontalalignment='center', verticalalignment='center')
  
 
