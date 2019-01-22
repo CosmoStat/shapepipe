59095058923e2745e8d3df5c040a376f1df5ffed
Axel <axel.guinot.09@orange.fr>
2019-01-22 15:30:36 +0100

    Bug fix

--- shapepipe/modules/__init__.py
+++ shapepipe/modules/__init__.py
@@ -9,4 +9,4 @@ This module contains sub-modules that can be run with ShapePipe.
 """
 
 __all__ = ['module_decorator', 'module_runners']
-__module_list__ = ['python_example', 'execute_example', 'sextractor_runner']
+__module_list__ = ['python_example', 'execute_example', 'mask_runner', 'sextractor_runner']
