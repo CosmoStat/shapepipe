5c0434966c0c05a4770060b6d1e8aaf506266f0d
Axel <axel.guinot.09@orange.fr>
2019-01-22 22:58:25 +0100

    Add suffix option for the output

--- shapepipe/modules/mask_runner.py
+++ shapepipe/modules/mask_runner.py
@@ -26,8 +26,9 @@ def mask_runner(input_file_list, output_dir, file_number_string,
         raise ValueError("Input files must be 'image', 'weight' and 'ext_flags' (optionnal)")
     
     config_file = config.get('MASK_RUNNER', 'MASK_CONFIG_PATH')
+    suffix = config.get('MASK_RUNNER', 'SUFFIX')
 
-    inst = mask(*input_file_list[:2], file_number_string, config_file, output_dir, path_external_flag=ext_flag_name)
+    inst = mask(*input_file_list[:2], suffix.replace(" ",""), file_number_string, config_file, output_dir, path_external_flag=ext_flag_name)
     stdout, stderr = inst.make_mask()
 
     return stdout, stderr
