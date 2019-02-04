d643934f908a0da35db8b8608a3f2b21f5cee591
Samuel Farrens <samuel.farrens@gmail.com>
2019-02-04 15:02:06 +0100

    updated list of modules

--- shapepipe/modules/__init__.py
+++ shapepipe/modules/__init__.py
@@ -9,11 +9,12 @@ This module contains sub-modules that can be run with ShapePipe.
 """
 
 __all__ = ['module_decorator', 'module_runners', 'find_exposures_package',
-           'mask_package', 'SETools_package']
+           'mask_package', 'PSFExInterpolation_package', 'SETools_package']
 __module_list__ = ['execute_example',
                    'find_exposures_runner',
                    'mask_runner',
                    'psfex_runner',
+                   'psfexinterp_runner',
                    'python_example',
                    'setools_runner',
                    'sextractor_runner',
