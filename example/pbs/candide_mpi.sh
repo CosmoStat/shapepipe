758602e4b3d8caba23cb9863205eee4d34f6bbc4
Samuel Farrens <samuel.farrens@gmail.com>
2019-02-14 17:20:19 +0100

    updated candide mpi script

--- example/pbs/candide_mpi.sh
+++ example/pbs/candide_mpi.sh
@@ -12,16 +12,18 @@
 # Join output and errors in one file
 #PBS -j oe
 # Request number of cores
-#PBS -l select=4:ncpus=1
+#PBS -l nodes=n04:ppn=10+n05:ppn=10
+#PBS -l walltime=00:05:00
 NSLOTS=`cat $PBS_NODEFILE | wc -l`
 
 # Activate conda environment
 module load intelpython/3
+module load openmpi/4.0.0
 source activate $HOME/.conda/envs/shapepipe
 
 # Run ShapePipe
 cd $HOME/ShapePipe
-mpiexec -n $NSLOTS $HOME/.conda/envs/shapepipe/bin/python shapepipe_run.py -c example/pbs/config_mpi.ini
+/softs/openmpi/4.0.0-torque-CentOS7/bin/mpiexec -n $NSLOTS $HOME/.conda/envs/shapepipe/bin/python shapepipe_run.py -c example/pbs/config_mpi.ini
 
 # Return exit code
 exit 0
