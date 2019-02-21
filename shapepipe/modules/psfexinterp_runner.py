be283c54ff49349feaf25a50f4570855fd82decb
Axel <axel.guinot.09@orange.fr>
2019-02-21 09:20:13 +0100

    Update PSFExInterp for multi-epoch

--- shapepipe/modules/psfexinterp_runner.py
+++ shapepipe/modules/psfexinterp_runner.py
@@ -19,14 +19,34 @@ from shapepipe.modules.PSFExInterpolation_package import interpolation_script
 def psfexinterp_runner(input_file_list, output_dir, file_number_string,
                        config, w_log):
 
-    psfcat_path, galcat_path = input_file_list
+    mode = config.get('PSFEXINTERP_RUNNER', 'MODE')
 
     pos_params = config.getlist('PSFEXINTERP_RUNNER', 'POSITION_PARAMS')
     get_shapes = config.getboolean('PSFEXINTERP_RUNNER', 'GET_SHAPES')
+    star_thresh = config.getint('PSFEXINTERP_RUNNER', 'STAR_THRESH')
 
-    inst = interpolation_script.PSFExInterpolator(psfcat_path, galcat_path,
-                                                  output_dir, file_number_string,
-                                                  pos_params, get_shapes)
-    inst.write_output()
+    if mode == 'C':
+        psfcat_path, galcat_path = input_file_list
+
+        inst = interpolation_script.PSFExInterpolator(psfcat_path, galcat_path,
+                                                      output_dir, file_number_string,
+                                                      pos_params, get_shapes, star_thresh)
+        inst.process()
+
+    elif mode == 'ME':
+        dot_psf_dir = config.getexpanded('PSFEXINTERP_RUNNER', 'ME_DOT_PSF_DIR')
+        dot_psf_pattern = config.get('PSFEXINTERP_RUNNER', 'ME_DOT_PSF_PATTERN')
+        f_wcs_path = config.getexpanded('PSFEXINTERP_RUNNER', 'ME_LOG_WCS')
+
+        galcat_path = input_file_list[0]
+
+        inst = interpolation_script.PSFExInterpolator(None, galcat_path,
+                                                      output_dir, file_number_string,
+                                                      pos_params, get_shapes, star_thresh)
+
+        inst.process_me(dot_psf_dir, dot_psf_pattern, f_wcs_path)
+
+    else:
+        ValueError('MODE has to be in : [C, ME]')
 
     return None, None
