5cf5d3e408a5fdc789b6509edcb0cdf21539a68f
Axel Guinot <axel.guinot@cea.fr>
2019-02-26 14:58:04 +0100

    Psfexinterp update

--- shapepipe/modules/PSFExInterpolation_package/interpolation_script.py
+++ shapepipe/modules/PSFExInterpolation_package/interpolation_script.py
@@ -31,7 +31,10 @@ else:
 from shapepipe.pipeline import file_io as sc
 
 
-def interpsfex(dotpsfpath, pos):
+NOT_ENOUGH_STARS = 'Fail'
+
+
+def interpsfex(dotpsfpath, pos, thresh):
     """Use PSFEx generated model to perform spatial PSF interpolation.
 
         Parameters
@@ -42,14 +45,22 @@ def interpsfex(dotpsfpath, pos):
         pos : np.ndaray
             Positions where the PSF model should be evaluated.
 
+        thresh : int
+            Threshold of stars under which the PSF is not interpolated
+
         Returns
         -------
         PSFs : np.ndarray
             Each row is the PSF imagette at the corresponding asked position.
-    """
 
+    """
     # read PSF model and extract basis and polynomial degree and scale position
     PSF_model = fits.open(dotpsfpath)[1]
+
+    # Check number of stars used to compute the PSF
+    if PSF_model.header['ACCEPTED'] < thresh:
+        return NOT_ENOUGH_STARS
+
     PSF_basis = np.array(PSF_model.data)[0][0]
     try:
         deg = PSF_model.header['POLDEG1']
@@ -98,11 +109,15 @@ class PSFExInterpolator(object):
         Desired position parameters. If provided, there should be exactly two,
         and they must also be present in the galaxy catalog. Otherwise,
         they are read directly from the .psf file.
+    get_shapes : boolean
+        If True will compute shapes for the PSF model.
+    star_thresh : int
+        Threshold of stars under which the PSF is not interpolated.
 
     """
 
-    def __init__(self, dotpsf_path, galcat_path, output_path, img_number,
-                 pos_params=None, get_shapes=True):
+    def __init__(self, dotpsf_path, galcat_path, output_path, img_number, w_log,
+                 pos_params=None, get_shapes=True, star_thresh=20):
 
         # Path to PSFEx output file
         self._dotpsf_path = dotpsf_path
@@ -110,6 +125,13 @@ class PSFExInterpolator(object):
         self._galcat_path = galcat_path
         # Path to output file to be written
         self._output_path = output_path+'/galaxy_psf'
+        # if required, compute and save shapes
+        self._compute_shape = get_shapes
+        # Number of stars under which we don't interpolate the PSF
+        self._star_thresh = star_thresh
+
+        # Logging
+        self._w_log = w_log
 
         # handle provided, but empty pos_params (for use within
         # CosmoStat's ShapePipe)
@@ -126,18 +148,29 @@ class PSFExInterpolator(object):
 
         self._img_number = img_number
 
-        # if required, compute and save shapes
-        if get_shapes:
-            self._get_psfshapes()
-            self._has_shapes = True
+    def process(self):
+        """ Process the PSF interpolation single-epoch run.
+
+        """
+        if self.gal_pos is None:
+            self._get_galaxy_positions()
+
+        if self.interp_PSFs is None:
+            self._interpolate()
+
+        if isinstance(self.interp_PSFs, str) and self.interp_PSFs == NOT_ENOUGH_STARS:
+            self._w_log.info('Not enough stars to interpolate the psf'
+                             ' in the file {}.'.format(self._dotpsf_path))
         else:
-            self._has_shapes = False
+            if self._compute_shape:
+                self._get_psfshapes()
+
+            self._write_output()
 
     def _get_position_parameters(self):
         """ Read position parameters from .psf file.
 
         """
-
         dotpsf = sc.FITSCatalog(self._dotpsf_path)
         dotpsf.open()
         self._pos_params = [dotpsf.get_header()['POLNAME1'],
@@ -174,15 +207,12 @@ class PSFExInterpolator(object):
         positions.)
 
         """
-        if self.gal_pos is None:
-            self._get_galaxy_positions()
-
         # pex = psfex.PSFEx(self._dotpsf_path)
         # self.interp_PSFs = np.array([pex.get_rec(x,y) for x,y in
         # zip(self.gal_pos[:,0],
         #                              self.gal_pos[:,1])])
 
-        self.interp_PSFs = interpsfex(self._dotpsf_path, self.gal_pos)
+        self.interp_PSFs = interpsfex(self._dotpsf_path, self.gal_pos, self._star_thresh)
 
     def _get_psfshapes(self):
         """ Compute shapes of PSF at galaxy positions using HSM.
@@ -191,32 +221,160 @@ class PSFExInterpolator(object):
         if import_fail:
             raise ImportError('Galsim is required to get shapes information')
 
-        if self.interp_PSFs is None:
-            self._interpolate()
         psf_moms = [hsm.FindAdaptiveMom(Image(psf), strict=False)
                     for psf in self.interp_PSFs]
 
         self.psf_shapes = np.array([[moms.observed_shape.g1,
                                      moms.observed_shape.g2,
-                                     moms.moments_sigma] for moms in psf_moms])
-        self.hsm_flags = np.array([bool(mom.error_message)
-                                   for mom in psf_moms]).astype(int)
+                                     moms.moments_sigma,
+                                     int(bool(moms.error_message))] for moms in psf_moms])
 
-    def write_output(self):
+    def _write_output(self):
         """ Save computed PSFs to fits file.
 
         """
-        if self.interp_PSFs is None:
-            self._interpolate()
         output = sc.FITSCatalog(self._output_path+self._img_number+'.fits',
                                 open_mode=sc.BaseCatalog.OpenMode.ReadWrite,
                                 SEx_catalog=True)
-        if self._has_shapes:
+
+        if self._compute_shape:
             data = {'VIGNET': self.interp_PSFs,
                     'E1_PSF_HSM': self.psf_shapes[:, 0],
                     'E2_PSF_HSM': self.psf_shapes[:, 1],
                     'SIGMA_PSF_HSM': self.psf_shapes[:, 2],
-                    'HSM_FLAG': self.hsm_flags}
+                    'HSM_FLAG': self.psf_shapes[:, 3].astype(int)}
         else:
             data = {'VIGNET': self.interp_PSFs}
         output.save_as_fits(data, sex_cat_path=self._galcat_path)
+
+    def process_me(self, dot_psf_dir, dot_psf_pattern, f_wcs_path):
+        """ Process the multi-epoch.
+
+        Parameters
+        ----------
+        dot_psf_dir : str
+            Path to the directory containing the ".psf" files.
+        dot_psf_pattern : str
+            Common pattern of the ".psf" files.
+        f_wcs_path : str
+            Path to the log file containing the WCS for each CCDs.
+
+        """
+        self._dot_psf_dir = dot_psf_dir
+        self._dot_psf_pattern = dot_psf_pattern
+        self._f_wcs_file = np.load(f_wcs_path).item()
+
+        if self.gal_pos is None:
+            self._get_galaxy_positions()
+
+        output_list = self._interpolate_me()
+
+        self._write_output_me(output_list)
+
+    def _interpolate_me(self):
+        """ Interpolate PSFs for multi-epoch run.
+
+        Returns
+        -------
+        list
+            List contianing object Ids, the interpolated PSFs and shapes (optionally)
+
+        """
+        cat = sc.FITSCatalog(self._galcat_path, SEx_catalog=True)
+        cat.open()
+
+        all_id = np.copy(cat.get_data()['NUMBER'])
+        n_epoch = np.copy(cat.get_data()['N_EPOCH'])
+
+        list_ext_name = cat.get_ext_name()
+        hdu_ind = [i for i in range(len(list_ext_name)) if 'EPOCH' in list_ext_name[i]]
+
+        final_list = []
+        for hdu_index in hdu_ind:
+            exp_name = cat.get_data(hdu_index)['EXP_NAME'][0]
+            ccd_list = list(set(cat.get_data(hdu_index)['CCD_N']))
+            array_psf = None
+            array_id = None
+            array_shape = None
+            for ccd in ccd_list:
+                if ccd == -1:
+                    continue
+                dot_psf_path = self._dot_psf_dir + '/' + self._dot_psf_pattern + '-' + exp_name + '-' + str(ccd) + '.psf'
+                ind_obj = np.where(cat.get_data(hdu_index)['CCD_N'] == ccd)[0]
+                obj_id = all_id[ind_obj]
+                gal_pos = np.array(self._f_wcs_file[exp_name][ccd].all_world2pix(self.gal_pos[:, 0][ind_obj], self.gal_pos[:, 1][ind_obj], 0)).T
+
+                self.interp_PSFs = interpsfex(dot_psf_path, gal_pos, self._star_thresh)
+
+                if isinstance(self.interp_PSFs, str) and self.interp_PSFs == NOT_ENOUGH_STARS:
+                    self._w_log.info('Not enough stars find in the ccd'
+                                     ' {} of the exposure {}. Object inside'
+                                     ' this ccd will lose an epoch.'.format(ccd, exp_name))
+                    continue
+
+                if array_psf is None:
+                    array_psf = np.copy(self.interp_PSFs)
+                else:
+                    array_psf = np.concatenate((array_psf, np.copy(self.interp_PSFs)))
+
+                if array_id is None:
+                    array_id = np.copy(obj_id)
+                else:
+                    array_id = np.concatenate((array_id, np.copy(obj_id)))
+
+                if self._compute_shape:
+                    self._get_psfshapes()
+                    if array_shape is None:
+                        array_shape = np.copy(self.psf_shapes)
+                    else:
+                        array_shape = np.concatenate((array_shape, np.copy(self.psf_shapes)))
+                else:
+                    array_shape = None
+
+            final_list.append([array_id, array_psf, array_shape])
+
+        cat.close()
+
+        output_list_id = [[] for i in range(max(n_epoch))]
+        output_list_vign = [[] for i in range(max(n_epoch))]
+        output_list_shape = [[] for i in range(max(n_epoch))]
+        for i in range(len(all_id)):
+            counter = 0
+            for j in range(len(final_list)):
+                where_res = np.where(final_list[j][0] == all_id[i])[0]
+
+                if (len(where_res) != 0):
+                    output_list_id[counter].append(final_list[j][0][where_res])
+                    output_list_vign[counter].append(final_list[j][1][where_res])
+                    if self._compute_shape:
+                        output_list_shape[counter].append(final_list[j][2][where_res])
+                    counter += 1
+
+        return [output_list_id, output_list_vign, output_list_shape]
+
+    def _write_output_me(self, output_list):
+        """ Save computed PSFs to fits file for multi-epoch run.
+
+        Parameters
+        ----------
+        output_list : list
+            List of outputs to save
+
+        """
+        output_file = sc.FITSCatalog(self._output_path+self._img_number+'.fits',
+                                     open_mode=sc.BaseCatalog.OpenMode.ReadWrite,
+                                     SEx_catalog=True)
+
+        for i in range(len(output_list[0])):
+            out_dict = {}
+            out_dict['NUMBER'] = np.array(output_list[0][i]).squeeze()
+            out_dict['VIGNET'] = np.array(output_list[1][i]).squeeze()
+            if self._compute_shape:
+                out_dict['E1_PSF_HSM'] = np.array(output_list[2][i]).squeeze()[:, 0]
+                out_dict['E2_PSF_HSM'] = np.array(output_list[2][i]).squeeze()[:, 1]
+                out_dict['SIGMA_PSF_HSM'] = np.array(output_list[2][i]).squeeze()[:, 2]
+                out_dict['HSM_FLAG'] = np.array(output_list[2][i]).squeeze()[:, 3].astype(int)
+
+            output_file.save_as_fits(out_dict,
+                                     ext_name='N_EPOCH_{}'.format(i+1),
+                                     sex_cat_path=self._galcat_path)
