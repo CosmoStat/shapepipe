1a9e65158c3781eefe9daa8ad4bfe41492760e09
Axel <axel.guinot.09@orange.fr>
2019-07-01 16:02:29 +0200

    Add script in the pipeline serial format
    Merge the star catalogs after the PSFExInterpolation run in "VALIDATION" mode

---
+++ shapepipe/modules/merge_star_cat_runner.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+
+"""CREATE LOG EXP HEADER
+
+This module merge the PSF validation catalogs from the PSFExInterpolation module
+to run the statistics on them.
+
+:Author: Morgan Schmitz, Axel Guinot
+
+"""
+
+
+import numpy as np
+from astropy.io import fits
+
+import os
+import re
+
+from shapepipe.modules.module_decorator import module_runner
+from shapepipe.pipeline import file_io as sc
+
+
+@module_runner(input_module='split_exp_runner', version='1.0',
+               file_pattern=['header'],
+               file_ext=['.npy'], depends=['numpy', 'sqlitedict'],
+               run_method='serial')
+def merge_star_cat_runner(input_file_list, output_dir, file_number_string,
+                          config, w_log):
+
+    # starcats_path = '/home/guinot/ShapePipe_dir/output_singles2/shapepipe_run_2019-03-29_15-46-39/psfexinterp_runner/output/'
+    # stub = 'validation_psf'
+
+    # Read starcats and save full
+    # starcat_names = os.listdir(starcats_path)
+    # starcat_names = [starcats_path+name for name in starcat_names if stub in name]
+
+    x, y, ra, dec = [], [], [], []
+    g1_psf, g2_psf, size_psf = [], [], []
+    g1, g2, size = [], [], []
+    flag_psf, flag_star = [], []
+    mag, snr, psfex_acc = [], [], []
+    ccd_nb = []
+
+
+    for name in input_file_list:
+        starcat_j = fits.open(name[0])
+
+        # positions
+        x += list(starcat_j[2].data['X'])
+        y += list(starcat_j[2].data['Y'])
+        ra += list(starcat_j[2].data['RA'])
+        dec += list(starcat_j[2].data['DEC'])
+
+        # shapes (convert sigmas to R^2)
+        g1_psf += list(starcat_j[2].data['E1_PSF_HSM'])
+        g2_psf += list(starcat_j[2].data['E2_PSF_HSM'])
+        size_psf += list(starcat_j[2].data['SIGMA_PSF_HSM']**2)
+        g1 += list(starcat_j[2].data['E1_STAR_HSM'])
+        g2 += list(starcat_j[2].data['E2_STAR_HSM'])
+        size += list(starcat_j[2].data['SIGMA_STAR_HSM']**2)
+
+        # flags
+        flag_psf += list(starcat_j[2].data['FLAG_PSF_HSM'])
+        flag_star += list(starcat_j[2].data['FLAG_STAR_HSM'])
+
+        # misc
+        mag += list(starcat_j[2].data['MAG'])
+        snr += list(starcat_j[2].data['SNR'])
+        psfex_acc += list(starcat_j[2].data['ACCEPTED'])
+
+        # CCD number
+        ccd_nb += [int(re.split(r"\-([0-9]*)\-([0-9]+)\.", name[0])[-2])]*len(starcat_j[2].data['RA'])
+
+    output = sc.FITSCatalog(output_dir + '/full_starcat.fits',
+                            open_mode=sc.BaseCatalog.OpenMode.ReadWrite)
+    # convert back to sigma for consistency
+    data = {'X': x, 'Y': y, 'RA': ra, 'DEC': dec,
+            'E1_PSF_HSM': g1_psf, 'E2_PSF_HSM': g2_psf, 'SIGMA_PSF_HSM': np.sqrt(size_psf),
+            'E1_STAR_HSM': g1, 'E2_STAR_HSM': g2, 'SIGMA_STAR_HSM': np.sqrt(size),
+            'FLAG_PSF_HSM': flag_psf, 'FLAG_STAR_HSM': flag_star,
+            'MAG': mag, 'SNR': snr, 'ACCEPTED': psfex_acc,
+            'CCD_NB': ccd_nb}
+    print('Writing full catalog...')
+    output.save_as_fits(data)
+    print('... Done.')
+
+    return None, None
