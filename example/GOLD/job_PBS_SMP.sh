d18f4566e9c32b6cd8fd2304e91b6a7ceb6e69ff
Martin Kilbinger <martin.kilbinger@cea.fr>
2020-05-12 16:24:50 +0200

    job_SGE_MPI.sh running on cc, tile

--- example/GOLD/job_PBS_SMP.sh
+++ example/GOLD/job_PBS_SMP.sh
@@ -25,16 +25,9 @@ fi
 echo -n "pwd = "
 pwd
 
-# Activate conda environment
-# The following is from Sam's example, does not work for Martin
-# (some library error)
-#ccenv anaconda
-#source activate $HOME/.conda/envs/shapepipe
-
 source ~/miniconda3/bin/activate shapepipe
 
 # Run ShapePipe
-#cmd="$HOME/.conda/envs/shapepipe/bin/python $HOME/ShapePipe/shapepipe_run.py -c $1"
 cmd="$HOME/miniconda3/envs/shapepipe/bin/python $HOME/ShapePipe/shapepipe_run.py -c $1"
 echo "Running command $cmd"
 $cmd
