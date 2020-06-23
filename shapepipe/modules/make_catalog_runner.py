65fc9248607f505b886dc125e637715e7a752360
Martin Kilbinger <martin.kilbinger@cea.fr>
2020-06-23 14:56:01 +0200

    cleaned up commented code bits

--- shapepipe/modules/make_catalog_runner.py
+++ shapepipe/modules/make_catalog_runner.py
@@ -269,59 +269,6 @@ def save_ngmix_mom_shapes(final_cat_file, ngmix_cat_path):
     ngmix_cat_file.close()
 
 
-#def save_galsim_shapes(final_cat_file, galsim_cat_path):
-    #""" Save ngmix data
-#
-    #Save the ngmix catalog into the final one.
-#
-    #Parameters
-    #----------
-    #final_cat_file : io.FITSCatalog
-        #Final catalog.
-    #ngmix_cat_path : str
-        #Path to ngmix catalog to save.
-#
-    #"""
-#
-    #final_cat_file.open()
-    #obj_id = np.copy(final_cat_file.get_data()['NUMBER'])
-#
-    #galsim_cat_file = io.FITSCatalog(galsim_cat_path)
-    #galsim_cat_file.open()
-    #galsim_id = galsim_cat_file.get_data()['id']
-    ## max_epoch = np.max(ngmix_n_epoch)
-#
-    #output_dict = {'GALSIM_GAL_ELL': np.ones((len(obj_id), 2)) * -10.,
-                   #'GALSIM_GAL_ELL_ERR': np.ones(len(obj_id)) * -1.,
-                   #'GALSIM_GAL_SIGMA': np.zeros(len(obj_id)),
-                   #'GALSIM_GAL_FLAG': np.ones(len(obj_id), dtype='int16'),
-                   #'GALSIM_GAL_ELL_U': np.ones((len(obj_id), 2)) * -10.,
-                   #'GALSIM_GAL_RES': np.ones(len(obj_id)) * -1.,
-                   #'GALSIM_PSF_ELL': np.ones((len(obj_id), 2)) * -10.,
-                   #'GALSIM_PSF_SIGMA': np.zeros(len(obj_id)),
-                   #'GALSIM_PSF_FLAG': np.ones(len(obj_id), dtype='int16')}
-    #for i, id_tmp in enumerate(obj_id):
-        #ind = np.where(id_tmp == galsim_id)[0]
-        #if len(ind) > 0:
-            #output_dict['GALSIM_GAL_ELL'][i][0] = galsim_cat_file.get_data()['gal_g1'][ind[0]]
-            #output_dict['GALSIM_GAL_ELL'][i][1] = galsim_cat_file.get_data()['gal_g2'][ind[0]]
-            #output_dict['GALSIM_GAL_ELL_ERR'][i] = galsim_cat_file.get_data()['gal_g1_err'][ind[0]]
-            #output_dict['GALSIM_GAL_SIGMA'][i] = galsim_cat_file.get_data()['gal_sigma'][ind[0]]
-            #output_dict['GALSIM_GAL_FLAG'][i] = galsim_cat_file.get_data()['gal_flag'][ind[0]]
-            #output_dict['GALSIM_GAL_RES'][i] = galsim_cat_file.get_data()['gal_resolution'][ind[0]]
-            #output_dict['GALSIM_GAL_ELL_U'][i][0] = galsim_cat_file.get_data()['gal_uncorr_g1'][ind[0]]
-            #output_dict['GALSIM_PSF_ELL'][i][0] = galsim_cat_file.get_data()['psf_g1'][ind[0]]
-            #output_dict['GALSIM_PSF_ELL'][i][1] = galsim_cat_file.get_data()['psf_g2'][ind[0]]
-            #output_dict['GALSIM_PSF_SIGMA'][i] = galsim_cat_file.get_data()['psf_sigma'][ind[0]]
-#
-    #for key in output_dict.keys():
-        #final_cat_file.add_col(key, output_dict[key])
-#
-    #final_cat_file.close()
-    #galsim_cat_file.close()
-
-
-
 def save_galsim_shapes(final_cat_file, galsim_cat_path):
     """ Save ngmix data
 
@@ -470,9 +417,10 @@ def make_catalog_runner(input_file_list, run_dirs, file_number_string,
             save_ngmix_data(final_cat_file, shape1_cat_path)
         elif shape_type.lower() == "galsim":
             w_log.info('Save galsim data')
-            #save_ngmix_mom_shapes(final_cat_file, shape2_cat_path)
             save_galsim_shapes(final_cat_file, shape2_cat_path)
 
+    # PSF data from PSFExInterpol module: Very slow (sql -> numpy
+    # transformation), and not required (ngmix has also PSF parameters)
     # w_log.info('Save PSF data')
     # save_psf_data(final_cat_file, galaxy_psf_path, w_log)
 
