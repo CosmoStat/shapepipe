50231ed884e5d088d77625c743c044532d0be7ea
mkilbing <martin.kilbinger@cea.fr>
2020-06-29 12:45:12 +0200

    merge_final_cat: added paramer file as cmd option

--- scripts/python/merge_final_cat.py
+++ scripts/python/merge_final_cat.py
@@ -19,15 +19,156 @@ import numpy as np
 import os
 import sys
 import re
-from tqdm import tqdm
+import copy
+
+from optparse import OptionParser
+
+#from tqdm import tqdm
+
+
+class param:
+    """General class to store (default) variables
+    """
+
+    def __init__(self, **kwds):
+        self.__dict__.update(kwds)
+
+    def print(self, **kwds):
+        print(self.__dict__)
+
+    def var_list(self, **kwds):
+        return vars(self)
+
+
+def params_default():
+    """Set default parameter values.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    p_def: class param
+        parameter values
+    """
+
+    p_def = param(
+        input_path  = '.',
+    )
+
+    return p_def
+
+
+def parse_options(p_def):
+    """Parse command line options.
+
+    Parameters
+    ----------
+    p_def: class param
+        parameter values
+
+    Returns
+    -------
+    options: tuple
+        Command line options
+    args: string
+        Command line string
+    """
+
+    usage  = "%prog [OPTIONS]"
+    parser = OptionParser(usage=usage)
+
+    parser.add_option('-i', '--input_path', dest='input_path', type='string',
+                      default=p_def.input_path,
+                      help='input path, default=\'{}\''.format(p_def.input_path))
+
+    parser.add_option('-v', '--verbose', dest='verbose', action='store_true', help='verbose output')
+
+    options, args = parser.parse_args()
+
+    return options, args
+
+
+def check_options(options):
+    """Check command line options.
+
+    Parameters
+    ----------
+    options: tuple
+        Command line options
+
+    Returns
+    -------
+    erg: bool
+        Result of option check. False if invalid option value.
+    """
+
+    return True
+
+
+def update_param(p_def, options):
+    """Return default parameter, updated and complemented according to options.
+
+    Parameters
+    ----------
+    p_def:  class param
+        parameter values
+    optiosn: tuple
+        command line options
+
+    Returns
+    -------
+    param: class param
+        updated paramter values
+    """
+
+    param = copy.copy(p_def)
+
+    # Update keys in param according to options values
+    for key in vars(param):
+        if key in vars(options):
+            setattr(param, key, getattr(options, key))
+
+    # Add remaining keys from options to param
+    for key in vars(options):
+        if not key in vars(param):
+            setattr(param, key, getattr(options, key))
+
+    # Do extra stuff if necessary
+
+    return param
+
+
+def get_data(path, hdu_num, param_list):
+
+    d = fits.getdata(lpath, hdu_num)
+
+    if param_list:
+        cols = []
+        for p in param_list:
+            cols.append(d_tmp.column[p]) 
+        coldefs = fits.ColDefs(cols)
+        hdu_new = fits.BinTableHDU.from_columns(coldefs)
+        d = hdu_new.data
+
+    return d
 
 
 def main(argv=None):
 
-    try:
-        path = argv[1]
-    except:
-        path = '.'
+    # Set default parameters
+    p_def = params_default()
+
+    # Command line options
+    options, args = parse_options(p_def)
+
+    if check_options(options) is False:
+        return 1
+
+    param = update_param(p_def, options)
+
+    path = param.input_path
 
     l = os.listdir(path=path)
     lpath = []
@@ -35,7 +176,8 @@ def main(argv=None):
         lpath.append(os.path.join(path, this_l))
 
     # Determine number of columns and keys
-    d_tmp = fits.getdata(lpath[0], 1)
+    d_tmp = get_data(lpath[0], 1, param.param_list)
+
     d = np.zeros(d_tmp.shape, dtype=d_tmp.dtype)
     for key in d_tmp.dtype.names:
         d[key] = d_tmp[key]
@@ -46,14 +188,17 @@ def main(argv=None):
     d['TILE_ID'].fill(int(''.join(re.findall('\d+', l[0]))))
 
     # Read all final catalogues and merge
-    for i in tqdm(lpath[1:], total=len(lpath)-1):
+    #for i in tqdm(lpath[1:], total=len(lpath)-1):
+    for i in lpath[1:]:
         if ('final_cat' not in i) | ('.npy' in i):
             continue
 
         #new_dt = np.dtype(d_tmp.dtype.descr + [('TILE_ID', '>i4')])
         #dd = np.zeros(d_tmp.shape, dtype=new_dt)
 
-        d_tmp = fits.getdata(i, 1)
+        #d_tmp = fits.getdata(i, 1)
+        d_tmp = get_data(i, 1, param.param_list)
+
         dd = np.zeros(d_tmp.shape, dtype=d_tmp.dtype)
         for key in d_tmp.dtype.names:
             dd[key] = d_tmp[key]
@@ -66,4 +211,3 @@ def main(argv=None):
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
-
