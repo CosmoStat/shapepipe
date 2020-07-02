6a4cc991538434eb67ff0fe1e3940796ddaea048
Martin Kilbinger <martin.kilbinger@cea.fr>
2020-07-02 15:03:23 +0000

    Updated canfar_submit_selection

--- scripts/sh/canfar_submit_selection.sh
+++ scripts/sh/canfar_submit_selection.sh
@@ -1,36 +1,17 @@
 #!/usr/bin/env bash
 
-# Name: submit_selection.sh
+# Name: canfar_submit_selection.sh
 # Author: Martin Kilbinger (2020), martin.kilbinger@cea.fr
 # Description: Submits jobs to canfar, each jobs processes
 #  single exposures for one tile
 # Package: ShapePipe
 
-## Variables
+
+# Variables
 
 # Job file for one tile
 SP_ROOT=$HOME/shapepipe
 sp_job="$SP_ROOT/scripts/sh/canfar_sp.bash"
-
-# Image/VM ID
-image="ShapePipe2-mk-20200701"
-
-
-if [ "$1" == "-h" ]; then
-  echo "Usage: submit_selection.sh [-n]"
-  echo "Options:"
-  echo "  -n       dry run"
-  exit 0
-fi
-
-
-if [ "$1" == "-n" ]; then
-  dry_run=1
-  dry_str=" (dry run)"
-else
-  dry_run=0
-  dry_str=""
-fi
 
 
 # Functions
@@ -55,7 +36,6 @@ function create_job_file() {
    add_queue $job_file
 }
 
-
 function add_to_job_file () {
    local tile_ID=$1
    local job_file=$2
@@ -65,7 +45,6 @@ function add_to_job_file () {
    echo "error          = log_canfar_sp_$tile_ID.err" >> $job_file
    echo "log            = log_canfar_sp_$tile_ID.log" >> $job_file
 }
-
 
 function finalize_job_file() {
    local job_file=$1
@@ -79,22 +58,46 @@ function finalize_job_file() {
    echo >> $job_file
 }
 
+function usage () {
+  ex=$1
+
+  echo "Usage: canfar_submit_selection.sh ID_path image [-h] [-n]"
+  echo "Options:"
+  echo "  ID_path       ascii file with tile IDs"
+  echo "  image         VM image name, newest is ShapePipe2-mk-20200701b"
+  echo "  -n            dry run"
+  echo "  -h            this message"
+
+  exit $ex
+}
+
+if [ "${!#}" == "-h" ]; then
+  usage 0
+fi
+if [ "$#" -lt 2 ] ; then
+  usage 1
+fi
+
+if [ "${!#}" == "-n" ]; then
+  dry_run=1
+  dry_str=" (dry run)"
+else
+  dry_run=0
+  dry_str=""
+fi
+
+
+## Start
+
+tile_ID_list=$1
+image=$2
+
 if [ $dry_run == 0 ]; then
    # TODO: Check whether the following directories exist
    # (e.g. error msg of vmkdir), remove previous run
    # results
    vmkdir vos:cfis/cosmostat/kilbinger/results
 fi
-
-
-# Input tile list file
-#tile_list_file="../w3_tile.txt"
-
-# Output tile ID list file
-tile_ID_list="tile_id_list.txt"
-
-# Create tile ID list
-#cat $tile_list_file | perl -ane '/CFIS\.(\d{3}\.\d{3})/; print "$1\n"' > $tile_ID_list
 
 
 # Create job file
