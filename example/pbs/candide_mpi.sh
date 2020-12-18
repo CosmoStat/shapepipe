7a73ad4b41465924dbb8956ed13ea315e7143e6a
Martin Kilbinger <martin.kilbinger@cea.fr>
2020-12-18 12:10:42 +0100

    candide mpi pbs job updated

--- example/pbs/candide_mpi.sh
+++ example/pbs/candide_mpi.sh
@@ -5,30 +5,36 @@
 ##########################
 
 # Receive email when job finishes or aborts
-#PBS -M <name>@cea.fr
-#PBS -m ea
+## #PBS -M <name>@cea.fr
+## #PBS -m ea
+
 # Set a name for the job
 #PBS -N shapepipe_mpi
+
 # Join output and errors in one file
 #PBS -j oe
+
 # Set maximum computing time (e.g. 5min)
 #PBS -l walltime=00:05:00
-# Request number of cores (e.g. 4 from 2 different machines)
+
+# Request number of cores (e.g. 2 from 2 different machines)
 #PBS -l nodes=2:ppn=2
-# Allocate total number of cores to variable NSLOTS
-NSLOTS=`cat $PBS_NODEFILE | wc -l`
 
 # Full path to environment
 export SPENV="$HOME/.conda/envs/shapepipe"
+
+# Full path to example config file and input data
 export SPDIR="$HOME/shapepipe"
 
-# Load moudules and activate conda environment
+# Load modules
 module load intelpython/3
-module load openmpi/4.0.2
+module load openmpi/4.0.5
+
+# Activate conda environment
 source activate $SPENV
 
 # Run ShapePipe using full paths to executables
-$SPENV/bin/mpiexec -n $NSLOTS $SPENV/bin/shapepipe_run -c $SPDIR/example/pbs/config_mpi.ini
+$SPENV/bin/mpiexec --map-by node $SPENV/bin/shapepipe_run -c $SPDIR/example/config_mpi.ini
 
 # Return exit code
 exit 0
