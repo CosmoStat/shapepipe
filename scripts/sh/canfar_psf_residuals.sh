d69f4fee413a1f673230b380df8c81edb7a56e60
Ubuntu <martin.kilbinger@cea.fr>
2021-02-01 17:36:09 +0000

    changes for mccd post_proc

--- scripts/sh/canfar_psf_residuals.sh
+++ scripts/sh/canfar_psf_residuals.sh
@@ -8,13 +8,52 @@
 
 
 
-## Paths
+## Default variables
 psfval_file_base="validation_psf"
 dir_individual="psf_validation_ind"
 dir_merged="psf_validation_merged"
 fname_merged="psf_cat_full.fits"
 pwd=`pwd`
 
+# Command line sarguments
+
+## Help string
+usage="Usage: $(basename "$0") [OPTIONS]
+\n\nOptions:\n
+   -h\tthis message\n
+   -p, --psf MODEL\n
+    \tPSF model, one in ['psfex'|'mccd'], default='$psf'\n
+"
+
+## Parse command line
+TILE_ARR=()
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -h)
+      echo -ne $usage
+      exit 0
+      ;;
+    -p|--psf)
+      psf="$2"
+      shift
+      ;;
+    *)
+      echo "Invalid command line argument '$2'"
+      echo -ne $usage
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+
+if [ "$psf" == "psfex" ]; then
+  psfval_dir_base="psfex_interp"
+else
+  #psfval_dir_base="mccd_fit_val"
+  echo "psf=mccd not yet supported in this script"
+  exit 3
+fi
 
 ## Functions
 function link_s () {
@@ -22,12 +61,10 @@ function link_s () {
     link_name=$2
  
     if [ -L "$link_name" ]; then
-        #echo "link with name $link_name already exists, skipping..."
-	let "n_skipped+=1"
+	      let "n_skipped+=1"
     else
-        #echo "create link $target <- $link_name"
         ln -s $target $link_name
-	let "n_created+=1"
+	      let "n_created+=1"
     fi
 
     return $n
@@ -47,7 +84,7 @@ done
 # Assumes untar_results.sh has been run before.
 n_skipped=0
 n_created=0
-FILES=output/*/psfex_interp_runner/output/${psfval_file_base}*
+FILES=output/*/${psfval_dir_base}_runner/output/${psfval_file_base}*
 for val in $FILES; do
     base=`basename $val`
     link_s "$pwd/$val" "$dir_individual/$base"
