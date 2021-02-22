0eda3edf1030a4700e6c6461117acdb0e44d7276
Martin Kilbinger <martin.kilbinger@cea.fr>
2021-02-22 17:12:51 +0100

    paste_cat update

--- shapepipe/modules/paste_cat_runner.py
+++ shapepipe/modules/paste_cat_runner.py
@@ -2,7 +2,7 @@
 
 """PASTE CAT RUNNER
 
-This module pastes different (SExtractor) catalogs of objects with identical IDs.
+Pipeline runner for the PasteCat package.
 
 :Author: Martin Kilbinger <martin.kilbinger@cea.fr>, Axel Guinot
 
@@ -13,94 +13,14 @@ This module pastes different (SExtractor) catalogs of objects with identical IDs
 """
 
 from shapepipe.modules.module_decorator import module_runner
-from shapepipe.pipeline import file_io as io
-
-import os
-import re
-
-import numpy as np
-
-from astropy import units as u
-from astropy import coordinates as coords
-from astropy.wcs import WCS
+from shapepipe.modules.PastCat_package import PastCat_script as paste
 
 
-class PasteCat(object):
-    """PasteCat initialisation.
-
-    Parameters
-    ----------
-    input_file_list : list of strings
-        list of input catalogue paths to be pasted.
-    output_path : string
-        output file path of pasted catalog
-    w_log :
-        log file
-    ext_name : list of strings, optional, default=None
-        HDU extension names, if None use input file names
-    check_col_name : string, optional, default=None:
-        if not None, use column with this key to check equal number
-        of rows in each input catalog
-    hdu_no : array of int, optional, default=None
-        hdu numbers of input catalog; by default set to 2 for all
-        input files
-    """
-
-    def __init__(self, input_file_list, output_path, w_log,
-                 ext_name=None, check_col_name=None, hdu_no=None):
-
-        self._input_file_list = input_file_list
-        self._output_path = output_path
-        self._w_log = w_log
-        self._ext_name = ext_name
-        self._check_col_name = check_col_name
-        if hdu_no is None:
-            self._hdu_no = [2] * len(input_file_list)
-        else:
-            self._hdu_no = hdu_no
-
-    def process(self):
-
-        # Create output catalog
-        pasted_cat = io.FITSCatalog(self._output_path,
-                                    open_mode=io.BaseCatalog.OpenMode.ReadWrite)
-
-        for i, input_file in enumerate(self._input_file_list):
-            self._w_log.info('Pasting catalog \'{}\''.format(input_file))
-
-            # Read input data
-            cat = io.FITSCatalog(input_file)
-            cat.open()
-            data = np.copy(cat.get_data(self._hdu_no[i]))
-            col_names = cat.get_col_names(self._hdu_no[i])
-            cat.close()
-
-            # Check equality
-            if self._check_col_name:
-                if i > 0:
-                    if self._check_col_name not in col_names:
-                        raise KeyError('CHECK_COL_NAME key \'{}\' not found in '
-                                       'input catalog'
-                                       ''.format(self._check_col_name))
-                    if not (data[self._check_col_name] == data_prev[self._check_col_name]).all():
-                        raise Exception('Column check using key \'{}\' failed for input catalogs '
-                                        '#{} and #{}'
-                                        ''.format(self._check_col_name, i-1, i))
-                data_prev = data
-
-            # Add to output cat
-            if self._ext_name:
-                ext_name = self._ext_name[i]
-            else:
-                ext_name = input_file
-            pasted_cat.save_as_fits(data, ext_name=ext_name)
-
-
-@module_runner(version='1.0',
+@module_runner(version='1.1',
                input_module='sextractor_runner',
                file_pattern='tile_sexcat',
                file_ext='.fits',
-               depends='numpy',
+               depends=['numpy', 'astrpy'],
                run_method='parallel')
 def paste_cat_runner(input_file_list, run_dirs, file_number_string,
                      config, w_log):
@@ -141,8 +61,8 @@ def paste_cat_runner(input_file_list, run_dirs, file_number_string,
                                       file_number_string,
                                       file_ext)
 
-    inst = PasteCat(input_file_list, output_path, w_log, ext_name=ext_name_list,
-                    check_col_name=check_col_name, hdu_no=hdu_no)
+    inst = paste.PasteCat(input_file_list, output_path, w_log, ext_name=ext_name_list,
+                          check_col_name=check_col_name, hdu_no=hdu_no)
 
     inst.process()
 
