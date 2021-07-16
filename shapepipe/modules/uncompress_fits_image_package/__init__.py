5f8c21bf6099a6eeada2ecba624aecad3dd331e6
Martin Kilbinger <martin.kilbinger@cea.fr>
2021-07-16 19:59:52 +0200

    uncompress revised

---
+++ shapepipe/modules/uncompress_fits_image_package/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+  
+"""UNCOMRESS FITS
+
+This module uncompress fits images and save them on a single hdu fits.
+
+:Author: Axel Guinot, Martin Kilbinger <martin.kilbinger@cea.fr>
+
+:Date: 2020
+
+:Package: ShapePipe
+
+"""
+
+__all__ = ['uncompress']
