192b69ca32a9c45d3983a64f3cf8261a646d21cd
Martin Kilbinger <martin.kilbinger@cea.fr>
2021-07-21 18:01:38 +0200

    sm develop new syntax

--- shapepipe/modules/spread_model_runner.py
+++ shapepipe/modules/spread_model_runner.py
@@ -2,7 +2,7 @@
 
 """SPREAD MODEL RUNNER
 
-This module computes the spread model.
+Module runner for ``spread_model``
 
 :Author: Axel Guinot
 
@@ -11,24 +11,33 @@ This module computes the spread model.
 """
 
 from shapepipe.modules.module_decorator import module_runner
-from shapepipe.modules.SpreadModel_package import SpreadModel_script as sm
-
+from shapepipe.modules.spread_mdel_package import spread_model as sm
 
 
 @module_runner(
     version='1.0',
-    input_module=['sextractor_runner', 'psfex_interp_runner_me',
-                  'vignetmaker_runner'],
+    input_module=[
+        'sextractor_runner',
+        'psfex_interp_runner_me',
+        'vignetmaker_runner'
+    ],
     file_pattern=['sexcat', 'galaxy_psf', 'weight_vign'],
     file_ext=['.fits', '.sqlite', '.fits'],
     depends=['numpy', 'galsim'],
     run_method='parallel'
 )
-def spread_model_runner(input_file_list, run_dirs, file_number_string,
-                        config, w_log):
+def spread_model_runner(
+    input_file_list,
+    run_dirs,
+    file_number_string,
+    config,
+    w_log
+):
 
+    # Get input files
     sex_cat_path, psf_cat_path, weight_cat_path = input_file_list
 
+    # Get file suffix (optional)
     if config.has_option('SPREAD_MODEL_RUNNER', 'SUFFIX'):
         suffix = config.get('SPREAD_MODEL_RUNNER', 'SUFFIX')
         if (suffix.lower() != 'none') & (suffix != ''):
@@ -38,15 +47,25 @@ def spread_model_runner(input_file_list, run_dirs, file_number_string,
     else:
         suffix = ''
 
+    # Get pixel scale and output mode
     pixel_scale = config.getfloat('SPREAD_MODEL_RUNNER', 'PIXEL_SCALE')
     output_mode = config.get('SPREAD_MODEL_RUNNER', 'OUTPUT_MODE')
 
+    # Set output file path
     file_name = f'{suffix}sexcat_sm{file_number_string}.fits'
     output_path = f'{run_dirs["output"]}/{file_name}'
 
-    inst = sm.SpreadModel(sex_cat_path, psf_cat_path, weight_cat_path,
-                          output_path, pixel_scale, output_mode)
+    # Create spread model class instance
+    inst = sm.SpreadModel(
+        sex_cat_path,
+        psf_cat_path,
+        weight_cat_path,
+        output_path,
+        pixel_scale,
+        output_mode
+    )
 
+    # Process spread model computation
     inst.process()
 
     return None, None
