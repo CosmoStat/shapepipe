caf868390f6e843a3721131f87a1725a8fc6a480
Samuel Farrens <samuel.farrens@gmail.com>
2021-07-23 18:17:52 +0200

    firt pass on clean up

--- shapepipe/modules/setools_runner.py
+++ shapepipe/modules/setools_runner.py
@@ -2,26 +2,44 @@
 
 """SETOOLS RUNNER
 
-This file is the pipeline runner for the SETools package.
+Module runner for ``setools``.
 
 :Author: Axel Guinot
 
 """
 
 from shapepipe.modules.module_decorator import module_runner
-from shapepipe.modules.SETools_package import SETools_script as setools
+from shapepipe.modules.setools_package import setools
 
 
-@module_runner(input_module='sextractor_runner_exp', version='1.0',
-               file_pattern=['sexcat'], file_ext=['.fits'],
-               depends=['numpy', 'matplotlib'])
-def setools_runner(input_file_list, run_dirs, file_number_string,
-                   config, w_log):
+@module_runner(
+    input_module='sextractor_runner_exp',
+    version='1.0',
+    file_pattern=['sexcat'],
+    file_ext=['.fits'],
+    depends=['numpy', 'matplotlib']
+)
+def setools_runner(
+    input_file_list,
+    run_dirs,
+    file_number_string,
+    config,
+    w_log,
+):
 
+    # Get path to setools configuration file
     config_file = config.getexpanded('SETOOLS_RUNNER', 'SETOOLS_CONFIG_PATH')
 
-    inst = setools.SETools(input_file_list[0], run_dirs['output'],
-                           file_number_string, config_file)
-    inst.process(w_log)
+    # Create instance of SETools
+    se_inst = setools.SETools(
+        input_file_list[0],
+        run_dirs['output'],
+        file_number_string,
+        config_file,
+    )
 
+    # Process inputs
+    se_inst.process(w_log)
+
+    # No return objects
     return None, None
