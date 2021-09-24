697bec3da6ecbc5290e7b171ae7766dd9a6b4eed
Samuel Farrens <samuel.farrens@gmail.com>
2021-09-24 16:56:54 +0200

    first draft of cleaned match external module

---
+++ shapepipe/modules/match_external_package/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""MATCH EXTERNAL PACKAGE
+
+This package contains the module(s) for ``match_external``.
+
+:Author: Martin Kilbinger, Xavier Jimenez
+
+"""
+
+__all__ = ['match_external']
