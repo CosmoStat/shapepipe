712a1e69ef35034e872446745577448b29f763e3
Martin Kilbinger <martin.kilbinger@cea.fr>
2021-09-30 19:18:19 +0200

    added and tested merge_sep_cats module (#461)
    * added and tested merge_sep_cats module
    
    * Modifications according to PR review
    
    * Modifications according to PR review

---
+++ shapepipe/modules/merge_sep_cats_package/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""MERGE SEP CAT PACKAGE
+
+This package contains the module for ``merge_sep_cat``.
+
+:Author: Martin Kilbinger <martin.kilbinger@cea.fr>
+
+"""
+
+__all__ = ['merge_script.py']
