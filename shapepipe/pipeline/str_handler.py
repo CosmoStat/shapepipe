5452b16f418c1411f65e218a41f4451885aa76db
Martin Kilbinger <martin.kilbinger@cea.fr>
2021-12-16 17:27:17 +0100

    bug fixed in str_interpreter

--- shapepipe/pipeline/str_handler.py
+++ shapepipe/pipeline/str_handler.py
@@ -575,7 +575,7 @@ class StrInterpreter(object):
             if len(str_split) == 3:
                 if str_split[1] in self._mask.keys():
                     try:
-                        return self._cat[s[0]][self._mask[str_split[1]]]
+                        return self._cat[str_split[0]][self._mask[str_split[1]]]
                     except Exception:
                         raise ValueError(
                             'String has to be a catalogue parameter. '
