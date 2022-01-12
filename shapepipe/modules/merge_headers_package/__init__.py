af444f49121b00e209be93352225c9e871101ccd
Martin Kilbinger <martin.kilbinger@cea.fr>
2022-01-12 16:47:01 +0100

    doc merge headers

--- shapepipe/modules/merge_headers_package/__init__.py
+++ shapepipe/modules/merge_headers_package/__init__.py
@@ -6,6 +6,23 @@ This package contains the module(s) for ``merge_headers``.
 
 :Author: Axel Guinot
 
+:Parent module: split_exp_runner
+
+:Input: npy files with single-exposure header information
+
+:Output: single SQL file with combined header information
+
+Description
+===========
+
+This pipeline module merges the WCS information (image transformation and
+distortions, computed during astrometrical calibration) for each CCD of
+single-exposure images. The merged information is saved as a single SQL file.
+
+Module-specific config file entries
+===================================
+OUTPUT_PATH : str, optional
+    overrides the module output dir under ``[FILE]:OUTPUT_DIR``
 """
 
 __all__ = ['merge_headers']
