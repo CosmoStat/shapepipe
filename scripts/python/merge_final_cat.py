f2e53eb99d6c279f7f89d96a74fd2abae7306fa4
Martin Kilbinger <martin.kilbinger@cea.fr>
2022-01-13 09:07:21 +0100

    removed raising exception in merge_cat

--- scripts/python/merge_final_cat.py
+++ scripts/python/merge_final_cat.py
@@ -332,7 +332,6 @@ def main(argv=None):
             d = np.concatenate((d, dd))
         except:
             print(f'Error while adding file \'{idx}\', {len(dd)} objects not in final cat')
-            raise
 
     # Save merged catalogue as numpy binary file
     if param.verbose:
