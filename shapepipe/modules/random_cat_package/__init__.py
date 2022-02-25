dac2ecab3eac11e6e25980aad4b2cb153d6db326
Samuel Farrens <samuel.farrens@gmail.com>
2022-02-25 17:06:26 +0100

    first draft of docs for random_cat

--- shapepipe/modules/random_cat_package/__init__.py
+++ shapepipe/modules/random_cat_package/__init__.py
@@ -4,17 +4,28 @@ This package contains the module for ``random_cat``.
 
 :Author: Martin Kilbinger <martin.kilbinger@cea.fr>
 
-:Parent module:
+:Parent module: None
 
-:Input:
+:Input: images and masks
 
-:Output:
+:Output: random catalogue FITS file
 
 Description
 ===========
 
+This module creates a random catalogue, and computes the tile area accounting
+for overlapping and masked regions.
+
 Module-specific config file entries
 ===================================
+
+N_RANDOM : float
+    the number of random objects requested on output
+DENSITY : bool, optional
+    option to interpret the number of random objects per square degree, the
+    default is ``False``
+TILE_LIST : str, optional
+    path to tile IDs for overlap flagging
 
 """
 
