ee32b7f5b2e67fcf51d975618c3ed5a4cfd4bd5f
Tobias Liaudat <tobiasliaudat@gmail.com>
2022-02-28 09:46:45 +0100

    Style fix to pass the ci tests

--- shapepipe/modules/mccd_package/__init__.py
+++ shapepipe/modules/mccd_package/__init__.py
@@ -20,17 +20,17 @@ MCCD_PREPROCESSING_RUNNER
 
 :Input: star catalogue
 
-:Output: preprocessed star catalogue 
+:Output: preprocessed star catalogue
 
 This module preprocesses an input star catalog as required by the MCCD PSF model.
-As the MCCD PSF model builds one PSF model for the entire focal plane it requires that the different 
-single-CCD files be merged into a single focal plane file. 
+As the MCCD PSF model builds one PSF model for the entire focal plane it requires that the different
+single-CCD files be merged into a single focal plane file.
 The coordinates are transformed in order to have a global coordinate system.
-The minimum number of stars required to keep the CCD can be specified inside the MCCD config 
+The minimum number of stars required to keep the CCD can be specified inside the MCCD config
 file with the ``min_n_stars`` parameter.
 
 The input of this runner depends on the value of ``MODE``:
-  - ``FIT``: the path to the training star catalogue which will be used to fit the PSF models. 
+  - ``FIT``: the path to the training star catalogue which will be used to fit the PSF models.
   - ``VALIDATION``: the path to the validation star catalogue.
   - ``FIT_VALIDATION``: the path to the training and validation star catalogues, in that order.
 
@@ -39,9 +39,9 @@ Module-specific config file entries:
     - CONFIG_PATH: str
         path to the MCCD config file
     - MODE: str
-        mode in which the MCCD PSF model will run. 
+        mode in which the MCCD PSF model will run.
         It can be ``FIT``, ``VALIDATION`` or ``FIT_VALIDATION``.
-        With ``FIT``, the training dataset is preprocessed, with 
+        With ``FIT``, the training dataset is preprocessed, with
         ``VALIDATION`` the validation dataset is preprocessed, and with
         ``FIT_VALIDATION`` both are preprocessed
     - VERBOSE: bool
@@ -59,20 +59,20 @@ MCCD_RUNNERS
 
 :Output: fitted PSF models and or PSF validation star catalogue
 
-The three modules ``mccd_fit_runner``, ``mccd_val_runner`` and ``mccd_fit_val_runner`` handle the 
+The three modules ``mccd_fit_runner``, ``mccd_val_runner`` and ``mccd_fit_val_runner`` handle the
 fitting and the validation of the MCCD PSF models.
 
 They all share the same configuration file entries, although the inputs differ as follows
   - ``[MCCD_FIT_RUNNER]``: Has as input the path to the star catalogue that will be used to train the PSF models. The star catalogue should be already preprocessed by ``[MCCD_PREPROCESSING_RUNNER]``.
   - ``[MCCD_VAL_RUNNER]``: Has two inputs. First, the path to the fitted PSF models, and second, the path to the preprocessed validation star catalogue.
-  - ``[MCCD_FIT_VAL_RUNNER]``: Has two inputs. First, the path to the preprocessed training star catalogue that will be used to train the PSF models. Second, the path to the preprocessed validation star catalogue. 
+  - ``[MCCD_FIT_VAL_RUNNER]``: Has two inputs. First, the path to the preprocessed training star catalogue that will be used to train the PSF models. Second, the path to the preprocessed validation star catalogue.
 
 
 Module-specific config file entries:
     - CONFIG_PATH: str
         path to the MCCD config file
     - MODE: str
-        mode in which the MCCD PSF model will run. 
+        mode in which the MCCD PSF model will run.
         It can be ``FIT``, ``VALIDATION`` or ``FIT_VALIDATION``, and should match the used runner
     - VERBOSE: bool
         verbose mode
@@ -97,8 +97,8 @@ Module-specific config file entries:
         run mode for module, options are ``CLASSIC`` and ``MULTI-EPOCH``
     - POSITION_PARAMS: (list) str
         list of position parameter value names in the SExtractor output catalogue.
-        When running in multi-epoch those position have to be WCS. 
-        
+        When running in multi-epoch those position have to be WCS.
+
         For example:
 
         ``MULTI-EPOCH`` case:
@@ -131,7 +131,7 @@ MCCD_PLOTS_RUNNER
 
 This module generates different plots of the input merged star catalogue.
 The plots include meanshapes, where the ellipticity error is plotted as a function of the focal plane positions.
-The errors are binned through all the merged catalogue in the grid defined by ``X_GRID`` and ``Y_GRID``. 
+The errors are binned through all the merged catalogue in the grid defined by ``X_GRID`` and ``Y_GRID``.
 The other plots are a histogram of the ellipticity errors and the rho stats plot.
 
 
@@ -169,4 +169,3 @@ __all__ = [
     'mccd_interpolation_script',
     'mccd_plot_utilities',
 ]
-
