4e502154401c4349d9aa38103b01f7f3629ff272
Samuel Farrens <samuel.farrens@gmail.com>
2022-03-01 18:10:34 +0100

    added draft of docs for merge_starcat

--- shapepipe/modules/merge_starcat_package/__init__.py
+++ shapepipe/modules/merge_starcat_package/__init__.py
@@ -4,17 +4,23 @@ This package contains the module for ``merge_starcat``.
 
 :Author: Tobias Liaudat, Martin Kilbinger
 
-:Parent module:
+:Parent module: ``mccd_fit_val_runner``
 
-:Input:
+:Input: PSFEx or MCCD star catalogues
 
-:Output:
+:Output: merged star catalogue
 
 Description
 ===========
 
+This module merged the star catalogues used for building either PSFEx or MCCD
+PSF models.
+
 Module-specific config file entries
 ===================================
+
+PSF_MODEL : str
+    PSF model used; options are ``psfex`` or ``mccd``
 
 """
 
