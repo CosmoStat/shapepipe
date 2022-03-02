c80ea19ae7de1b6869918198d10625563133ac1e
Martin Kilbinger <martin.kilbinger@cea.fr>
2022-03-02 11:47:19 +0100

    Added merge_sep_cats API doc (#529)
    * Added merge_sep_cats API doc
    
    * fixed grammar
    
    * passing tests
    
    * Update shapepipe/modules/merge_sep_cats_package/__init__.py
    
    Co-authored-by: Samuel Farrens <samuel.farrens@gmail.com>
    
    Co-authored-by: Samuel Farrens <samuel.farrens@gmail.com>

--- shapepipe/modules/merge_sep_cats_package/__init__.py
+++ shapepipe/modules/merge_sep_cats_package/__init__.py
@@ -4,18 +4,33 @@ This package contains the module for ``merge_sep_cat``.
 
 :Author: Martin Kilbinger <martin.kilbinger@cea.fr>
 
-:Parent module:
+:Parent module: ``ngmix_runner``
 
-:Input:
+:Input: SExtractor catalogue
 
-:Output:
+:Output: SExtractor catalogue
 
 Description
 ===========
 
+This module merges separate input catalogues. This applies to
+a run of the previous module that was split into several (parallel)
+sub-processes to save wall-clock time. This is useful for time-consuming
+modules, such as galaxy shape measurement e.g. via ``ngmix_runner``. This
+module merges the outputs of those sub-processes back into a complete output
+catalogue.
+
+Note that the input catalogues should not contain identical objects.
+
 Module-specific config file entries
 ===================================
 
+N_SPLIT_MAX : int
+    Number of input sub-catalogues
+WARNING : str, optional
+    Warning action if one or more input catalogue is missing, default is
+    ``'error'``; one of the actions defined 
+    `here <https://docs.python.org/3/library/warnings.html#warning-filter>`_
 """
 
 __all__ = ['merge_sep_cats.py']
