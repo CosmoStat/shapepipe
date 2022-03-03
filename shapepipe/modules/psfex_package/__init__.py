eb1cb6dbdf606e4a47df413be8949def309931fc
Samuel Farrens <samuel.farrens@gmail.com>
2022-03-03 18:42:13 +0100

    draft of docs for PSFEx

--- shapepipe/modules/psfex_package/__init__.py
+++ shapepipe/modules/psfex_package/__init__.py
@@ -4,17 +4,33 @@ This package contains the module for ``psfex``.
 
 :Authors: Axel Guinot, Tobias Liaudat
 
-:Parent module:
+:Parent modules:
+- ``sextractor_runner``
+- ``setools_runner``
 
-:Input:
+:Input: SExtractor catalogue
 
-:Output:
+:Output: PSFEx PSF model
 
 Description
 ===========
 
+This module calls the community standard point spread function (PSF) modelling
+software PSFEx :cite:`bertin:11` to build a PSF model.
+
+This module requires a previous run of SExtractor.
+
 Module-specific config file entries
 ===================================
+
+EXEC_PATH : str
+    full path to the PSFEx executable on the system; if not set the
+    version controlled PSFEx installation in the ShapePipe environment
+    will be used
+DOT_PSFEX_FILE : str
+     full path to the ``.psfex`` configuration file for PSFEx
+CHECKIMAGE : list, optional
+     list of header key names corresponding to check images
 
 """
 
