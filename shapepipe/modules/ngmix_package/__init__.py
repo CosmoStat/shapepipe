6df67ace297dee4e3407d152f102904822af04f2
Samuel Farrens <samuel.farrens@gmail.com>
2022-03-04 13:30:04 +0100

    draft of docs for ngmix

--- shapepipe/modules/ngmix_package/__init__.py
+++ shapepipe/modules/ngmix_package/__init__.py
@@ -9,21 +9,32 @@ This package contains the module for ``ngmix``.
 - ``psfex_interp_runner``
 - ``vignetmaker_runner``
 
-:Input:
+:Input: galaxy image vignets
 
-:Output:
+:Output: shape catalogue
 
 Description
 ===========
+
+This module calls the weak-lensing shape measurment and metacalibration
+software NGMIX :cite:`sheldon:15`. In particular the GalSim :cite:`rowe:15`
+fitting method of NGXMIX is called to obtain galaxy shape measurements.
+The metacalibration routines of NGMIX are also called to provide all of the
+measurements required to calibrate the shear values.
 
 Module-specific config file entries
 ===================================
 
 MAG_ZP : float
+    photometric zero point
 PIXEL_SCALE : float
+    pixel scale in arcseconds
 LOG_WCS : str
+    path to world coordinate system log file (``*sqlite``)
 ID_OBJ_MIN : int
+    ID of first galaxy object to be processed
 ID_OBJ_MAX : int
+    ID of last galaxy object to be processed
 
 """
 
