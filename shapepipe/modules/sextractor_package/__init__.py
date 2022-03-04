ee0a60a12f80c4bd16b8b4c27cbc6de31f4899d1
Samuel Farrens <samuel.farrens@gmail.com>
2022-03-04 13:36:45 +0100

    capitalised first words

--- shapepipe/modules/sextractor_package/__init__.py
+++ shapepipe/modules/sextractor_package/__init__.py
@@ -25,45 +25,45 @@ Module-specific config file entries
 ===================================
 
 EXEC_PATH : str, optional
-    full path to the SExtractor executable on the system; if not set the
+    Full path to the SExtractor executable on the system; if not set the
     version controlled SExtractor installation in the ShapePipe environment
     will be used
 DOT_SEX_FILE : str
-    full path to the ``.sex`` configuration file for SExtractor
+    Full path to the ``.sex`` configuration file for SExtractor
 DOT_PARAM_FILE : str
-    full path to the ``.param`` configuration file for SExtractor
+    Full path to the ``.param`` configuration file for SExtractor
 DOT_CONV_FILE : str
-    full path to the ``.conv`` kernel file for SExtractor
+    Full path to the ``.conv`` kernel file for SExtractor
 WEIGHT_IMAGE : bool
-    option to specify if weight files have been provided
+    Option to specify if weight files have been provided
 FLAG_IMAGE : bool
-    option to specify if flag files have been provided
+    Option to specify if flag files have been provided
 PSF_FILE : bool
-    option to specify if PSF files have been provided
+    Option to specify if PSF files have been provided
 DETECTION_IMAGE : bool
-    option to specify if detection images have been provided
+    Option to specify if detection images have been provided
 DETECTION_WEIGHT : bool
-    option to specify if detection weights have been provided
+    Option to specify if detection weights have been provided
 ZP_FROM_HEADER : bool
-    option to to use magnitude zero points defined in the image headers
+    Option to to use magnitude zero points defined in the image headers
 ZP_KEY : str, optional
-    name of header key for magnitude zero points
+    Name of header key for magnitude zero points
 BKG_FROM_HEADER : bool
-    option to to use background levels defined in the image headers
+    Option to to use background levels defined in the image headers
 BKG_KEY : str, optional
-    name of header key for background levels
+    Name of header key for background levels
 CHECKIMAGE : list, optional
-    list of header key names corresponding to check images
+    List of header key names corresponding to check images
 PREFIX : str, optional
-    output file name prefix
+    Output file name prefix
 MAKE_POST_PROCESS : bool
-    option to run post-processing steps
+    Option to run post-processing steps
 LOG_WCS : str, optional
-    path to world coordinate system log file (``*sqlite``)
+    Path to world coordinate system log file (``*sqlite``)
 WORLD_POSITION : list, optional
-    list of world coordinates to use to match objects
+    List of world coordinates to use to match objects
 CCD_SIZE : list, optional
-    size of a CCD in pixels [nx, ny]
+    Size of a CCD in pixels [nx, ny]
 
 """
 
