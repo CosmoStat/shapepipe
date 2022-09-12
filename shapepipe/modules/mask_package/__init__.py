5d97ccecec997238450dec9f7cbe02add89f120f
Martin Kilbinger <martin.kilbinger@cea.fr>
2022-09-12 14:41:41 +0200

    added symlink

--- shapepipe/modules/mask_package/__init__.py
+++ shapepipe/modules/mask_package/__init__.py
@@ -23,7 +23,8 @@ distinguish the newly created output flag file from the input ones, a prefix
 can added as specificed by the config entry ``PREFIX``.
 
 An NGC catalogue with positions, sizes, and types is provided with
-``shapepipe``, source: www.klima-luft.de/steinicke/ngcic/rev2000/Explan.htm
+``shapepipe``,
+`source <www.klima-luft.de/steinicke/ngcic/rev2000/Explan.htmr>`_
 
 Masked pixels of different mask types are indicated by integers, which
 conveniently are powers of two such that they can be combined bit-wise.
