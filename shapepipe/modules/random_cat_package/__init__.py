c36c3edfc61cb5f87dfd72fe9e6520c8e52f9b4b
Martin Kilbinger <martin.kilbinger@cea.fr>
2022-10-05 11:49:07 +0200

    Update shapepipe/modules/random_cat_package/__init__.py
    Co-authored-by: Samuel Farrens <samuel.farrens@gmail.com>

--- shapepipe/modules/random_cat_package/__init__.py
+++ shapepipe/modules/random_cat_package/__init__.py
@@ -28,7 +28,7 @@ SAVE_MASK_AS_HEALPIX : bool
     Output healpix mask if ``True``
 HEALPIX_OUT_FILE_BASE : str, optional
     Output halpix mask file base name; used only if SAVE_MASK_AS_HEALPIX is
-    True
+    ``True``
 HEALPIX_OUT_NSIDE : int, optional
     Output healpix mask nside; used only if SAVE_MASK_AS_HEALPIX is True
 
