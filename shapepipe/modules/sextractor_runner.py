d327cc4f0540332928c4d80cb94d90b0c89fc2bf
Martin Kilbinger <martin.kilbinger@cea.fr>
2023-05-17 17:57:22 +0200

    sextractor_runner restored to develop

--- shapepipe/modules/sextractor_runner.py
+++ shapepipe/modules/sextractor_runner.py
@@ -36,93 +36,68 @@ def sextractor_runner(
     else:
         exec_path = 'sex'
 
-    if exec_path != 'None':
+    # Get SExtractor config options
+    dot_sex = config.getexpanded(module_config_sec, 'DOT_SEX_FILE')
+    dot_param = config.getexpanded(module_config_sec, 'DOT_PARAM_FILE')
+    dot_conv = config.getexpanded(module_config_sec, 'DOT_CONV_FILE')
+    weight_file = config.getboolean(module_config_sec, 'WEIGHT_IMAGE')
+    flag_file = config.getboolean(module_config_sec, 'FLAG_IMAGE')
+    psf_file = config.getboolean(module_config_sec, 'PSF_FILE')
+    detection_image = config.getboolean(module_config_sec, 'DETECTION_IMAGE')
+    detection_weight = config.getboolean(module_config_sec, 'DETECTION_WEIGHT')
 
-        # Get SExtractor config options
-        dot_sex = config.getexpanded(module_config_sec, 'DOT_SEX_FILE')
-        dot_param = config.getexpanded(module_config_sec, 'DOT_PARAM_FILE')
-        dot_conv = config.getexpanded(module_config_sec, 'DOT_CONV_FILE')
-        weight_file = config.getboolean(module_config_sec, 'WEIGHT_IMAGE')
-        flag_file = config.getboolean(module_config_sec, 'FLAG_IMAGE')
-        psf_file = config.getboolean(module_config_sec, 'PSF_FILE')
-        detection_image = config.getboolean(module_config_sec, 'DETECTION_IMAGE')
-        detection_weight = config.getboolean(module_config_sec, 'DETECTION_WEIGHT')
-
-        zp_from_header = config.getboolean(module_config_sec, 'ZP_FROM_HEADER')
-        if zp_from_header:
-            zp_key = config.get(module_config_sec, 'ZP_KEY')
-        else:
-            zp_key = None
-
-        bkg_from_header = config.getboolean(module_config_sec, 'BKG_FROM_HEADER')
-        if bkg_from_header:
-            bkg_key = config.get(module_config_sec, 'BKG_KEY')
-        else:
-            bkg_key = None
-
-        if config.has_option(module_config_sec, 'CHECKIMAGE'):
-            check_image = config.getlist(module_config_sec, 'CHECKIMAGE')
-        else:
-            check_image = ['']
-
-        if config.has_option(module_config_sec, 'PREFIX'):
-            prefix = config.get(module_config_sec, 'PREFIX')
-        else:
-            prefix = None
-
-        # Create sextractor caller class instance
-        ss_inst = ss.SExtractorCaller(
-            input_file_list,
-            run_dirs['output'],
-            file_number_string,
-            dot_sex,
-            dot_param,
-            dot_conv,
-            weight_file,
-            flag_file,
-            psf_file,
-            detection_image,
-            detection_weight,
-            zp_from_header,
-            bkg_from_header,
-            zero_point_key=zp_key,
-            background_key=bkg_key,
-            check_image=check_image,
-            output_prefix=prefix,
-        )
-
-        # Generate sextractor command line
-        command_line = ss_inst.make_command_line(exec_path)
-        w_log.info(f'Calling command: {command_line}')
-
-        # Execute command line
-        stderr, stdout = execute(command_line)
-
-        # Parse SExtractor errors
-        stdout, stderr = ss_inst.parse_errors(stderr, stdout)
-
+    zp_from_header = config.getboolean(module_config_sec, 'ZP_FROM_HEADER')
+    if zp_from_header:
+        zp_key = config.get(module_config_sec, 'ZP_KEY')
     else:
+        zp_key = None
 
-        # Read input catalogue
+    bkg_from_header = config.getboolean(module_config_sec, 'BKG_FROM_HEADER')
+    if bkg_from_header:
+        bkg_key = config.get(module_config_sec, 'BKG_KEY')
+    else:
+        bkg_key = None
+
+    if config.has_option(module_config_sec, 'CHECKIMAGE'):
+        check_image = config.getlist(module_config_sec, 'CHECKIMAGE')
+    else:
         check_image = ['']
-        ss_inst = ss.SExtractorCaller(
-            input_file_list,
-            run_dirs['output'],
-            file_number_string,
-            None,
-            None,
-            None,
-            True,
-            False,
-            False,
-            False,
-            False,
-            False,
-            False,
-            check_image=check_image,
-        )
-        data = ss_inst.read_from_ascii()
-        ss_inst.write_to_fits(data)
+
+    if config.has_option(module_config_sec, 'PREFIX'):
+        prefix = config.get(module_config_sec, 'PREFIX')
+    else:
+        prefix = None
+
+    # Create sextractor caller class instance
+    ss_inst = ss.SExtractorCaller(
+        input_file_list,
+        run_dirs['output'],
+        file_number_string,
+        dot_sex,
+        dot_param,
+        dot_conv,
+        weight_file,
+        flag_file,
+        psf_file,
+        detection_image,
+        detection_weight,
+        zp_from_header,
+        bkg_from_header,
+        zero_point_key=zp_key,
+        background_key=bkg_key,
+        check_image=check_image,
+        output_prefix=prefix,
+    )
+
+    # Generate sextractor command line
+    command_line = ss_inst.make_command_line(exec_path)
+    w_log.info(f'Calling command: {command_line}')
+
+    # Execute command line
+    stderr, stdout = execute(command_line)
+
+    # Parse SExtractor errors
+    stdout, stderr = ss_inst.parse_errors(stderr, stdout)
 
     # Run sextractor post processing
     if config.getboolean(module_config_sec, 'MAKE_POST_PROCESS'):
