0b3aac6277bedfbacd926e4f82004faf67c66615
Martin Kilbinger <martin.kilbinger@cea.fr>
2023-11-19 19:05:02 +0100

    script to call curl with canfar contained updated, adding NCORE as argument

--- scripts/sh/curl_canfar.sh
+++ scripts/sh/curl_canfar.sh
@@ -1,24 +1,30 @@
 #!/usr/bin/bash
 
 # Usage
-# cat tile_num.txt | xargs -n 1 -P 1 ~/astro/repositories/github/shapepipe/scripts/sh/curl_canfar.sh 0.7 shapepipe/scripts/sh/init_canfar.sh
+~/astro/repositories/github/shapepipe/scripts/sh/curl_canfar.sh 0.7 shapepipe/scripts/sh/init_canfar.sh 000.000
 
 SSL=~/.ssl/cadcproxy.pem
-SESSION=https://ws-uv.canfar.net/skaha/v0/session
+NCORE=16
+SESSION=https://ws-uv.canfar.net/skaha/v0/session?ram=16&cores=$NCORE
 IMAGE=images.canfar.net/unions/shapepipe
 NAME=shapepipe
 
+# version of image on canfar, e.g. 0:7, 0:8
 version=$1
-cmd=$2
-arg=$3
 
-arg_an=`echo "$arg" | tr '_' 'X' | tr '.' 'X'`
+# command on canfar, e.g. shapepipe/scripts/sh/init_run_canfar.sh
+cmd=$2
+
+# command line argument, e.g. 181.308
+arg="$3 $NCORE"
+
+#arg_an=`echo "$arg" | tr '_' 'X' | tr '.' 'X'`
 arg_an=X
 
+echo
 echo "Start headless container"
 echo "========================"
 ID=`curl -E $SSL $SESSION -d "image=$IMAGE:$version" -d "name=${NAME}${arg_an}" -d "cmd=$cmd" --data-urlencode "args=$arg"`
-#curl -E $SSL $SESSION -d \"name=$NAME\" -d \"image=images.canfar.net/unions/shapepipe\:0.7\" -d \"cmd=$2\" --data-urlencode \"args=$3\"
 echo $ID
 
 echo
