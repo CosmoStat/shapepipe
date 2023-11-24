d02347ea52885a170ffb09a4e462965fd70f729e
Martin Kilbinger <martin.kilbinger@cea.fr>
2023-11-24 10:51:16 +0100

    curl script exclusive ID

--- scripts/sh/curl_canfar.sh
+++ scripts/sh/curl_canfar.sh
@@ -1,31 +1,33 @@
 #!/usr/bin/bash
 
 # Usage
-# ~/astro/repositories/github/shapepipe/scripts/sh/curl_canfar.sh 0.7 shapepipe/scripts/sh/init_canfar.sh 000.000
+# ~/astro/repositories/github/shapepipe/scripts/sh/curl_canfar.sh 0.8 shapepipe/scripts/sh/init_run_exlusive_canfar.sh ID exp NCORE
 
 SSL=~/.ssl/cadcproxy.pem
-NCORE=16
+NCORE=2
 SESSION=https://ws-uv.canfar.net/skaha/v0/session
-RESOURCES="ram=16&cores=$NCORE"
+RESOURCES="ram=4&cores=$NCORE"
 IMAGE=images.canfar.net/unions/shapepipe
 NAME=shapepipe
 
 # version of image on canfar, e.g. 0:7, 0:8
 version=$1
 
-# command on canfar, e.g. shapepipe/scripts/sh/init_run_canfar.sh
+# command on canfar, e.g. shapepipe/scripts/sh/init_run_exclusive_canfar.sh
 cmd=$2
 
-# command line argument, e.g. 181.308
-arg="$3 $NCORE"
+# Image ID
+ID=$3
+
+# command line arguments
+arg="$ID $NCORE exp"
 
 #arg_an=`echo "$arg" | tr '_' 'X' | tr '.' 'X'`
-arg_an=X
 
 echo
 echo "Start headless container"
 echo "========================"
-ID=`curl -E $SSL $SESSION?$RESOURCES -d "image=$IMAGE:$version" -d "name=${NAME}${arg_an}" -d "cmd=$cmd" --data-urlencode "args=$arg"`
+ID=`curl -E $SSL $SESSION?$RESOURCES -d "image=$IMAGE:$version" -d "name=${NAME}" -d "cmd=$cmd" --data-urlencode "args=$arg"`
 echo $ID
 
 echo
