bd82f95ae96043c2de6ac577dae311c74a03e133
Martin Kilbinger <martin.kilbinger@cea.fr>
2023-12-14 21:35:06 +0100

    curl local command: added -k kind

--- scripts/sh/curl_canfar_local.sh
+++ scripts/sh/curl_canfar_local.sh
@@ -4,9 +4,9 @@
 # ~/astro/repositories/github/shapepipe/scripts/sh/curl_canfar.sh 0.9 shapepipe/scripts/sh/init_run_exlusive_canfar.sh ID ind
 
 SSL=~/.ssl/cadcproxy.pem
-NCORE=2
+N_SMP=2
 SESSION=https://ws-uv.canfar.net/skaha/v0/session
-RESOURCES="ram=4&cores=$NCORE"
+RESOURCES="ram=4&cores=$N_SMP"
 IMAGE=images.canfar.net/unions/shapepipe
 NAME=shapepipe
 
@@ -22,9 +22,9 @@ kind=$3
 # Image ID; has to be last argument to work with xargs
 ID=$4
 
-
-# command line arguments
-arg="$ID $NCORE $kind"
+# command line arguments for remote script:
+# collect into string
+arg="-j $JOB -e $ID -n $N_SMP -k $kind"
 
 ID=`curl -E $SSL $SESSION?$RESOURCES -d "image=$IMAGE:$version" -d "name=${NAME}" -d "cmd=$cmd" --data-urlencode "args=$arg"`
 echo $ID >> IDs.txt
