14569b28b75d4b283260f9f6e146f44123010623
Martin Kilbinger <martin.kilbinger@cea.fr>
2024-03-06 18:57:41 +0100

    corrected typo in doc string

--- shapepipe/pipeline/args.py
+++ shapepipe/pipeline/args.py
@@ -131,7 +131,7 @@ def create_arg_parser():
     optional.add_argument(
         '-e',
         '--exclusive',
-        help='exclusive input filer number string',
+        help='exclusive input file number string',
     )
     # Return parser
     return parser.parse_args()
