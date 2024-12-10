2e6cba6058868b5c3030ff6d4a340e1539420698
martinkilbinger <martin.kilbinger@cea.fr>
2024-12-10 15:28:57 +0000

    create hdf5: -l flag

--- scripts/python/create_final_cat.py
+++ scripts/python/create_final_cat.py
@@ -7,6 +7,8 @@
 Create and update hdf5 file of all final ShapePipe output FITS files, runs of
 ShapePipe module ``make_catalogue_runner``. Supercedes `merge_final_cat.py`.
 
+Usage: in parent dir of patches:
+create_final_cat.py -p ~/shapepipe/example/cfis/final_cat.param -i . -P 7 -v -m final_cat_P7.hdf5
 :Author: Martin Kilbinger
 
 """
@@ -60,22 +62,30 @@ def params_default():
     """                                                                     
     _params = {                                                        
         "input_root_dir": ".",
-        "output_path": "final_cat.hdf5",
+        "merged_cat_path": "final_cat.hdf5",
         "param_path": None,
         "hdu_num": 1,
+        "patch": "3",
+        "list_only": False,
     }
     _short_options = {                                                 
         "input_root_dir": "-i",                                           
-        "output_path": "-o",                                                 
+        "merged_cat_path": "-m",
         "param_path": "-p",
+        "patch": "-P",
+        "list_only": "-l",
+
     }                                                                       
     _types = {                                                         
         "hdu_num": "int",                                                   
+        "list_only": "bool",
     }                                                                       
     _help_strings = {                                                  
-        "input_root_dir": "input root_dir for tile catalogues",     
-        "output_path": "output path (hdf5 file)",
-        "param_path": "parameter file path, if not given use all columns",
+        "input_root_dir": "input root_dir for tile catalogues, default={}",
+        "merged_cat_path": "merged catalogue path (hdf5 file), default={}",
+        "param_path": "parameter file path, if not given use all columns, default={}",
+        "patch": "patch number, default={}",
+        "list_only": "print list of patches and IDs only, default={}",
     }
 
     return _params, _short_options, _types, _help_strings
@@ -136,10 +146,20 @@ def check_params(params):
     return True
 
 
+def print_list(params):
+
+    with h5py.File(params["merged_cat_path"], "a") as hdf5_file:
+
+        for patch in hdf5_file["patches"]:
+            print(patch)
+            for id in hdf5_file[f"patches/{patch}"]:
+                print(f" {id}")
+
+
 def process(params):
     
-    #patch_pattern = re.compile(r"P\d")  # Matches P followed by a single digit
-    patch_pattern = re.compile(r"P3")  # Matches P followed by a single digit
+    patch = rf"P{params['patch']}"
+    patch_pattern = re.compile(patch)
     
     # Define the nested path pattern for locating `.fits` files
     file_pattern = "output/run_sp_Mc_*/make_cat_runner/output/*.fits"
@@ -149,23 +169,27 @@ def process(params):
     
     # Open the HDF5 file (create it if it doesn't exist)
     if params["verbose"]:
-        print(f"Initializing file {params['output_path']}")
-    with h5py.File(params["output_path"], "a") as hdf5_file:
+        print(f"Initializing file {params['merged_cat_path']}")
+    with h5py.File(params["merged_cat_path"], "a") as hdf5_file:
 
         # Iterate over patches
         for patch in os.listdir(params["input_root_dir"]):
             if not patch_pattern.fullmatch(patch):  # Skip non-matching entries
+                #if params["verbose"]:
+                    #print(f"Patch {patch} not found in {params['input_root_dir']}, skipping")
                 continue
 
             patch_path = os.path.join(params["input_root_dir"], patch)
 
             if not os.path.isdir(patch_path):
+                if params["verbose"]:
+                    print(f"Path {patch_path} not found, skipping")
                 continue
         
             # Get or create a group for the patch
             if f"patches/{patch}" in hdf5_file:
                 if params["verbose"]:
-                    print(f"Extending group for patch {patch}")
+                    print(f"Processing group {patch}")
                 patch_group = hdf5_file[f"patches/{patch}"]
             else:
                 if params["verbose"]:
@@ -173,13 +197,16 @@ def process(params):
                 patch_group = hdf5_file.create_group(f"patches/{patch}")
         
             tile_runs_path = os.path.join(patch_path, "tile_runs")
-            #if params["verbose"]:
-                #print(f"Loop over tile IDs in {tile_runs_path}...")
             subdirs = os.listdir(tile_runs_path)
-            #if params["verbose"]:
-                #print(f"{len(subdirs)} IDs found")
                 
             for id in tqdm.tqdm(subdirs, total=len(subdirs)):
+
+                # Skip if the dataset already exists
+                if id in patch_group:
+                    if params["verbose"]:
+                        print(f"Skipping {id} (already processed)")
+                        continue
+
                 if id_pattern.match(id) and os.path.isdir(os.path.join(tile_runs_path, id)):
                     id_path = os.path.join(tile_runs_path, id)
                 
@@ -187,14 +214,7 @@ def process(params):
                     fits_files = glob.glob(output_path, recursive=True)
                     for fits_file in fits_files:
                     
-                        # Skip if the dataset already exists
-                        if id in patch_group:
-                            if params["verbose"]:
-                                print(f"Skipping {id} (already processed)")
-                                continue
-                    
-                        #if params["verbose"]:
-                            #print(f"Reading file {fits_file}...")
+                        # Read data
                         with fits.open(fits_file) as hdul:
                             data = hdul[params["hdu_num"]].data
 
@@ -204,8 +224,9 @@ def process(params):
 
                             try:
                                 extracted_data = {col: data[col] for col in params["param_list"]}
+                                dtype = data.dtype
                             except:
-                                print("Error for ID {id}. Keys not found:")
+                                print("Error for ID {id}")
                                 for col in params["param_list"]:
                                     if col not in data:
                                         print(col, end=" ")
@@ -213,17 +234,34 @@ def process(params):
                                 continue
 
                         # For columns with 2-entry quantities, ensure proper dtype
-                        dtype = []
-                        structured_data = np.empty(len(extracted_data[params["param_list"][0]]), dtype=dtype)
+                        #dtype = []
+                        structured_data = np.empty(
+                            len(extracted_data[params["param_list"][0]]),
+                            dtype=dtype,
+                        )
+
+                        # Loop over parameters
                         for col in params["param_list"]:
-                            if isinstance(extracted_data[col][0], (np.ndarray, tuple, list)):
+
+                            #if isinstance(extracted_data[col][0], (np.ndarray, tuple, list)):
+                            if False:
+
+                                continue
+                                # If multi-entry loop over entries
+                                print(col)
                                 num_elements = len(extracted_data[col][0])
                                 for idx in range(num_elements):
                                     new_col_name = f"{col}_{idx}"
                                     dtype.append((new_col_name, extracted_data[col].dtype))
-                                    structured_data[new_col_name] = [x[i] for x in extracted_data[col]]  # Extract element `i`
+                                    try:
+                                        structured_data[new_col_name] = [x[idx] for x in extracted_data[col]]  # Extract element `idx`
+                                    except:
+                                        print(f"Error for ID {id} for column {new_col_name}")
+                                        raise
                             else:
-                                dtype.append((col, extracted_data[col].dtype))
+                                # single entry
+                                if not col in extracted_data:
+                                    print(f"Column {col} not in file with ID {id}")
                                 structured_data[col] = extracted_data[col]
                         
                         # Create a new dataset
@@ -242,9 +280,14 @@ def main(argv=None):
 
     check_params(params)
 
-    params["param_list"] = read_param_file(params["param_path"], verbose=params["verbose"])
-
-    process(params)
+    if params["list_only"]:
+        print_list(params)
+    else:
+        params["param_list"] = read_param_file(
+            params["param_path"],
+            verbose=params["verbose"],
+        )
+        process(params)
 
     return 0
 
