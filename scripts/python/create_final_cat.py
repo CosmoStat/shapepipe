aff85a222bc8eb723dc878ebd736a2ec8de125e0
Martin Kilbinger <martin.kilbinger@cea.fr>
2025-01-24 09:47:29 +0100

    create final, version was lost from v1.4

--- scripts/python/create_final_cat.py
+++ scripts/python/create_final_cat.py
@@ -7,6 +7,7 @@ ShapePipe module ``make_catalogue_runner``. Supercedes `merge_final_cat.py`.
 
 Usage: in parent dir of patches:
 create_final_cat.py -p ~/shapepipe/example/cfis/final_cat.param -i . -P 7 -v -m final_cat_P7.hdf5
+
 :Author: Martin Kilbinger
 
 """
