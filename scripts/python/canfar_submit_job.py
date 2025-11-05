01ec68c49d54684fe588ccf0155f6e7657957b09
martinkilbinger <martin.kilbinger@cea.fr>
2025-11-05 15:42:13 +0000

    fixed bug for test mode

--- scripts/python/canfar_submit_job.py
+++ scripts/python/canfar_submit_job.py
@@ -18,7 +18,7 @@ class Job(object):
 
         self.params_default()
 
-        self._patch = os.environ["patch"]
+        self._patch = os.environ["patch"] if "patch" in os.environ else "P0"
 
 
     def set_params_from_command_line(self, args):
@@ -50,6 +50,7 @@ class Job(object):
             "n": "0",
             "debug_out": None,
             "test": 0,
+            "log": "log_job_tile_IDs.txt",
         }
 
         self._short_options = {
@@ -58,6 +59,7 @@ class Job(object):
             "file_IDs": "-f",
             "psf": "-p",
             "dry_run": "-n",
+            "log": "-l",
         }
 
         self._types = {
@@ -71,6 +73,7 @@ class Job(object):
             "psf": "PSF model, allowed are 'psfex' and 'mccd'; default is {}",
             "dry_run": "dry run, no actual processing",
             "debug_out": "debug output file path, default:set automatically",
+            "log": "output log file with job and tile IDs",
             "test": "test run level (2, 1, 0: no test, default)"
         }
 
@@ -82,6 +85,9 @@ class Job(object):
         pass
 
     def check_params(self):
+
+        if self._params["test"] == 2:
+            return
 
         if self._params["job"] == "-1":
             raise ValueError("No job indicated, use option -j")
@@ -103,17 +109,19 @@ class Job(object):
             opt["j"] = f"-j {self._params['job']}"
             opt["p"] = f"-p {self._params['psf']}"
 
-            if self._params["exclusive"]:
-                tile_ID = self._params["exclusive"]
+            # Set identified for debug file name
+            if self._params["test"] == 2:
+                deb_ID = "test"
+            elif self._params["exclusive"]:
+                deb_ID = self._params["exclusive"]
             else:
-                # Ony used for debug file name
-                tile_ID = self._params["file_IDs"]
+                deb_ID = self._params["file_IDs"]
 
             cwd = os.getcwd()
             if self._params['debug_out']:
                 opt["debut_out"] = f"--debug_out {self._params['debug_out']}"
             else:
-                opt["debut_out"] = f"--debug_out {cwd}/debug/debug_{tile_ID}.txt"
+                opt["debut_out"] = f"--debug_out {cwd}/debug/debug_{deb_ID}.txt"
 
             opt["d"] = f"-d {cwd}"
             opt["m_s"] = "-m 1 -s 1" if self._patch in ("P8", "P9") else ""
@@ -128,9 +136,6 @@ class Job(object):
         return options
 
     def set_job_name(self, tile_ID):
-
-        if self._params["test"] == 2:
-            return "sp-test"
 
         tile_ID_dash = tile_ID.replace(".", "-")
 
@@ -163,7 +168,9 @@ class Job(object):
 
         cmd = self.set_command()
 
-        if self._params["exclusive"]:
+        if self._params["test"] == 2:
+            tile_IDs = ["test"]
+        elif self._params["exclusive"]:
             tile_IDs = [self._params["exclusive"]]
         else:
             with open(self._params["file_IDs"], "r") as f:
@@ -190,8 +197,13 @@ class Job(object):
             print(f"Submitted job: {job_id}")
             job_ids.append(job_id)
 
-        return job_ids
+        return job_ids, tile_IDs
 
+    def write_job_tile_IDs(self, job_ids, tile_IDs):
+
+        with open(self._params["log"], "w") as log:
+            for job_id, tile_ID in zip(job_ids, tile_IDs):
+                print(job_id, tile_ID, file=log)
 
 def run_job(*args):
 
@@ -200,9 +212,12 @@ def run_job(*args):
 
     obj.set_params_from_command_line(args)
     obj.update_params()
+    obj.check_params()
 
-    job_ids = obj.run()
-    print("All job IDs: ", job_ids)
+    job_ids, tile_IDs = obj.run()
+    print(f"Submitting {len(job_ids)} jobs: done")
+
+    obj.write_job_tile_IDs(job_ids, tile_IDs)
 
 def main(argv=None):
     """Main
